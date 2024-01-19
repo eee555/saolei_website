a2873c2844c20de1fa4d95dd2d3b72bcea00a449
Tianyi Pu <912396513@qq.com>
2024-01-19 11:42:55 +0000

    增加一些教程链接

--- front_end/src/views/GuideView.vue
+++ front_end/src/views/GuideView.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="home">
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <h1>新手综合</h1>
+    <ul>
+      <li><a href="http://saolei.wang/BBS/Title.asp?Id=21006">新手向教学：综合实力提升——龚浩然</a></li>
+    </ul>
+    <h1>游戏理解</h1>
+    <ul>
+      <li><a href="http://saolei.wang/BBS/Title.asp?Id=12992">郭嘉谈NF——郭蔚嘉</a></li>
+      <li><a href="http://saolei.wang/BBS/Title.asp?Id=14967">浅谈NF破空——张少武</a></li>
+      <li><a href="https://zhuanlan.zhihu.com/p/27439584">初步认识减法公式及其衍生的最简单定式——MsPVZ.ZSW</a></li>
+      <li><a href="http://saolei.wang/BBS/Title.asp?Id=20660">竞速实战常用定式——濮天羿</a></li>
+      <li><a href="http://saolei.wang/BBS/Title.asp?Id=18971">经典角部2x2猜雷及其边部变式——郭蔚嘉</a></li>
+      <li><a href="http://saolei.wang/BBS/Title.asp?Id=14160">拐角判雷方法——林艺海</a></li>
+    </ul>
+    <h1>游戏数据</h1>
+    <ul>
+      <li><a href="http://saolei.wang/BBS/Title.asp?Id=6946">3BV与ZiNi浅析——周科</a></li>
+      <li><a href="http://saolei.wang/BBS/Title.asp?Id=21249">梁山分系统介绍——龚浩然</a></li>
+      <li><a href="https://www.bilibili.com/opus/880405709451165779">JSD大表实用指南——Arya-Noone</a></li>
+    </ul>
+    <h1>软件硬件</h1>
+    <ul>
+      <li><a href="http://saolei.wang/BBS/Title.asp?Id=283">将分辨率调成640x480的方法——张砷镓</a></li>
+      <li><a href="http://saolei.wang/BBS/Title.asp?Id=11145">Arbiter语法教程——肖旭升</a></li>
+    </ul>
   </div>
 </template>
 
