57a6bef00c1096027884727f636b25a6b6e5470b
Tianyi Pu <912396513@qq.com>
2024-04-06 15:51:44 +0100

    Update admin.py

--- back_end/saolei/videomanager/admin.py
+++ back_end/saolei/videomanager/admin.py
@@ -5,4 +5,4 @@ from .models import VideoModel
 @admin.register(VideoModel)
 class VideoAdmin(admin.ModelAdmin):
     # 指定后台网页要显示的字段
-    list_display = ("id", "mode", "state", "level", "player", "file", "upload_time", "state", "level", "rtime", "bv")
+    list_display = ("id", "mode", "state", "level", "player", "file", "upload_time", "state", "level", "milliseconds", "bv")
