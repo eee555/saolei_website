e002378589989743f87bb22644fc48df1ace5482
eee555 <2234208506@qq.com>
2024-04-19 12:58:11 +0800

    接入百度统计

--- back_end/sql/select_from.sql
+++ back_end/sql/select_from.sql
@@ -1,17 +1,18 @@
 #select * from `userprofile_emailverifyrecord`;
 #userprofile_emailverifyrecordselect * from `userprofile_userprofile`
-#select * from `videomanager_videomodel`
+select * from `videomanager_videomodel`
 # select * from `captcha_captchastore`
 #DELETE FROM `videomanager_expandvideomodel` where id Between 0 and 999
 #DELETE FROM `django_session` where expire_date Between '2021-09-21 10:26:34.415908' and '2023-09-21 10:26:34.415908'
 #select * from `userprofile_userprofile`
 #select * from `videomanager_expandvideomodel`
 #select * from `msuser_userms`
-#UPDATE userprofile_userprofile SET is_staff = 1 WHERE id = 4;
+#UPDATE userprofile_userprofile SET is_staff = 1 WHERE id = 5;
 #UPDATE userprofile_userprofile SET is_superuser = 1 WHERE id = 2;
 #UPDATE userprofile_userprofile SET left_realname_n = 999 WHERE id = 4
 #UPDATE userprofile_userprofile SET left_signature_n = 999 WHERE id = 1
-UPDATE userprofile_userprofile SET left_avatar_n = 999 WHERE id = 4
+#UPDATE userprofile_userprofile SET left_avatar_n = 999 WHERE id = 4
+#UPDATE msuser_userms SET designators = "[]" WHERE id = 2
 
 #select * from `userprofile_emailverifyrecord`
 #select * from `django_session`
