650ec998d1514b17476cd1653ddbef9ffc765ba8
Tianyi Pu <912396513@qq.com>
2024-04-28 10:53:04 +0100

    Update settings.py

--- back_end/saolei/saolei/settings.py
+++ back_end/saolei/saolei/settings.py
@@ -222,20 +222,6 @@ EMAIL_HOST_PASSWORD = "TdS2ZPt2izAkY25b"   # 密码 (注意：这里的密码指
 # EMAIL_USE_SSL = True
 # EMAIL_FROM = "wangjianing@88.com"      # 邮箱来自
 
-EMAIL_SKIP = False # 是否跳过邮箱验证。用于调试
-BAIDU_VERIFY_SKIP = False # 是否跳过审查步骤。用于调试
-DESIGNATOR_SKIP = False # 是否跳过标识检查。用于调试
-if EMAIL_SKIP or BAIDU_VERIFY_SKIP or DESIGNATOR_SKIP: # 以防万一忘了改
-    if EMAIL_SKIP:
-        print("已跳过邮箱验证步骤！\n")
-    if BAIDU_VERIFY_SKIP:
-        print("已跳过网络审查步骤！\n")
-    if DESIGNATOR_SKIP:
-        print("已跳过标识检查步骤！\n")
-    user_input = input("Do you want to continue? (y/n): ")
-    if user_input.lower() != 'y':
-        raise SystemExit("Program terminated.")
-
 SESSION_COOKIE_NAME = "session_id"        # Session的cookie保存在浏览器上时的key
 SESSION_COOKIE_PATH = "/"                # Session的cookie保存的路径(默认)
 SESSION_COOKIE_DOMAIN = None             # Session的cookie保存的域名(默认)
