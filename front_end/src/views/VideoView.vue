dedbe561c8b3b00de564afe918591a874b78aea9
Tianyi Pu <912396513@qq.com>
2024-04-29 07:13:10 +0100

    Update VideoView.vue

--- front_end/src/views/VideoView.vue
+++ front_end/src/views/VideoView.vue
@@ -18,9 +18,13 @@
     </el-row>
 
     <el-row class="mb-4" style="margin-bottom: 10px;">
-        <el-button v-for="(tag, key) in index_tags" type="primary" :plain="!(index_tag_selected == key)" :size="'small'"
-            @click="index_tag_selected = key as string; mod_style(); get_video_rank(1);">{{ tag.name
+        <el-button v-for="(value, key) in index_tags" type="primary" :plain="!value.selected" :size="'small'"
+            @click="console.log(123); index_tags[key].selected = !value.selected; ">{{ value.name
             }}</el-button>
+    </el-row>
+
+    <el-row class="mb-4" style="margin-bottom: 10px;">
+        <el-button size="small" @click="request_videos">筛选</el-button>
     </el-row>
 
     <div style="width: 80%;font-size:20px;margin: auto;margin-top: 10px;">
@@ -151,6 +155,7 @@ interface NameKeyReverse {
     key: string;
     reverse: boolean;
     to_fixed: number;
+    selected: boolean;
 }
 interface TagsReverse {
     [index: string]: NameKeyReverse;
@@ -178,31 +183,31 @@ const mode_tags: Tags = {
 
 
 // reverse: true从小到大
-const index_tags: TagsReverse = {
-    "upload_time": { name: "上传时间", key: "upload_time", reverse: true, to_fixed: -1 },
-    "timems": { name: "成绩", key: "timems", reverse: false, to_fixed: 3 },
-    "bbbv": { name: "3BV", key: "bv", reverse: false, to_fixed: 0 },
-    "bbbv_s": { name: "3BV/s", key: "bvs", reverse: true, to_fixed: 3 },
-    "left_s": { name: "left/s", key: "left_s", reverse: true, to_fixed: 3 },
-    "right_s": { name: "right/s", key: "right_s", reverse: true, to_fixed: 3 },
-    "double_s": { name: "double/s", key: "double_s", reverse: true, to_fixed: 3 },
-    "cl_s": { name: "cl/s", key: "cl_s", reverse: true, to_fixed: 3 },
-    "path": { name: "path", key: "path", reverse: false, to_fixed: 2 },
-    "stnb": { name: "STNB", key: "stnb", reverse: true, to_fixed: 2 },
-    "ioe": { name: "IOE", key: "ioe", reverse: true, to_fixed: 3 },
-    "thrp": { name: "ThrP", key: "thrp", reverse: true, to_fixed: 3 },
-    "ce_s": { name: "ce/s", key: "ce_s", reverse: true, to_fixed: 3 },
-    "op": { name: "空", key: "op", reverse: false, to_fixed: 0 },
-    "is": { name: "岛", key: "isl", reverse: false, to_fixed: 0 },
-    "cell1": { name: "1", key: "cell1", reverse: false, to_fixed: 0 },
-    "cell2": { name: "2", key: "cell2", reverse: false, to_fixed: 0 },
-    "cell3": { name: "3", key: "cell3", reverse: false, to_fixed: 0 },
-    "cell4": { name: "4", key: "cell4", reverse: false, to_fixed: 0 },
-    "cell5": { name: "5", key: "cell5", reverse: false, to_fixed: 0 },
-    "cell6": { name: "6", key: "cell6", reverse: false, to_fixed: 0 },
-    "cell7": { name: "7", key: "cell7", reverse: false, to_fixed: 0 },
-    "cell8": { name: "8", key: "cell8", reverse: false, to_fixed: 0 }
-}
+const index_tags: TagsReverse = reactive({
+    "upload_time": { name: "上传时间", key: "upload_time", reverse: true, to_fixed: -1, selected: true },
+    "timems": { name: "成绩", key: "timems", reverse: false, to_fixed: 3, selected: true },
+    "bbbv": { name: "3BV", key: "bv", reverse: false, to_fixed: 0, selected: true },
+    "bbbv_s": { name: "3BV/s", key: "bvs", reverse: true, to_fixed: 3, selected: true },
+    "left_s": { name: "left/s", key: "left_s", reverse: true, to_fixed: 3, selected: false },
+    "right_s": { name: "right/s", key: "right_s", reverse: true, to_fixed: 3, selected: false },
+    "double_s": { name: "double/s", key: "double_s", reverse: true, to_fixed: 3, selected: false },
+    "cl_s": { name: "cl/s", key: "cl_s", reverse: true, to_fixed: 3, selected: false },
+    "path": { name: "path", key: "path", reverse: false, to_fixed: 2, selected: false },
+    "stnb": { name: "STNB", key: "stnb", reverse: true, to_fixed: 2, selected: true },
+    "ioe": { name: "IOE", key: "ioe", reverse: true, to_fixed: 3, selected: false },
+    "thrp": { name: "ThrP", key: "thrp", reverse: true, to_fixed: 3, selected: false },
+    "ce_s": { name: "ce/s", key: "ce_s", reverse: true, to_fixed: 3, selected: false },
+    "op": { name: "空", key: "op", reverse: false, to_fixed: 0, selected: false },
+    "is": { name: "岛", key: "isl", reverse: false, to_fixed: 0, selected: false },
+    "cell1": { name: "1", key: "cell1", reverse: false, to_fixed: 0, selected: false },
+    "cell2": { name: "2", key: "cell2", reverse: false, to_fixed: 0, selected: false },
+    "cell3": { name: "3", key: "cell3", reverse: false, to_fixed: 0, selected: false },
+    "cell4": { name: "4", key: "cell4", reverse: false, to_fixed: 0, selected: false },
+    "cell5": { name: "5", key: "cell5", reverse: false, to_fixed: 0, selected: false },
+    "cell6": { name: "6", key: "cell6", reverse: false, to_fixed: 0, selected: false },
+    "cell7": { name: "7", key: "cell7", reverse: false, to_fixed: 0, selected: false },
+    "cell8": { name: "8", key: "cell8", reverse: false, to_fixed: 0, selected: false }
+})
 
 onMounted(() => {
     document.getElementsByClassName("el-pagination__goto")[0].childNodes[0].nodeValue = "转到";
@@ -233,6 +238,12 @@ const mod_style = () => {
 
     index_visible.value = !["upload_time", "bbbv", "bbbv_s", "timems"].
         includes(index_tag_selected.value);
+}
+
+const request_videos = () => {
+    for (var tag of Object.keys(index_tags)) {
+        
+    }
 }
 
 // 根据配置，刷新当前页面的录像表
