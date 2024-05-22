0dcd40061e917ccfb2ae0e206ca9df6b878e23e5
Tianyi Pu <912396513@qq.com>
2024-05-22 22:24:39 +0100

    Create LanguagePicker.vue

---
+++ front_end/src/components/LanguagePicker.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-select v-model="i18n.global.locale.value" style="width: 200px">
+        <el-option v-for="item in options" :key="item.lang" :label="item.text" :value="item.lang" />
+    </el-select>
+</template>
+
+<script lang="ts" setup name="LanguagePicker">
+
+import i18n from '@/i18n';
+
+const options = [
+    { lang: 'dev', text: 'dev' },
+    { lang: 'zh-cn', text: '简体中文' },
+]
+
+</script>
