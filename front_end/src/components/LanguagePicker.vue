30b23f56d41e22870db7479e19d01f234a13c71a
Tianyi Pu <912396513@qq.com>
2024-05-26 15:53:01 +0100

    Update LanguagePicker.vue

--- front_end/src/components/LanguagePicker.vue
+++ front_end/src/components/LanguagePicker.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-select v-model="i18n.global.locale.value" style="width: 200px">
+    <el-select v-model="i18n.global.locale.value" style="width: 100px">
         <el-option v-for="item in options" :key="item.lang" :label="item.text" :value="item.lang" />
     </el-select>
 </template>
@@ -7,10 +7,14 @@
 <script lang="ts" setup name="LanguagePicker">
 
 import i18n from '@/i18n';
+import { useI18n } from 'vue-i18n';
+const t = useI18n();
 
 const options = [
     { lang: 'dev', text: 'dev' },
     { lang: 'zh-cn', text: '简体中文' },
+    { lang: 'de', text: 'name'},
+    { lang: 'en', text: 'English'}
 ]
 
 </script>
