68a2624babf594fb0af517cb53a9d7042d78c481
Tianyi Pu <912396513@qq.com>
2024-06-17 12:07:21 +0100

    格式化LanguagePicker.vue

--- front_end/src/components/LanguagePicker.vue
+++ front_end/src/components/LanguagePicker.vue
@@ -3,38 +3,36 @@
         <el-image :src="logo_lang" style="width: 24px; height: 24px; margin-top: 18px"></el-image>
         <template #dropdown>
             <el-dropdown-item v-for="item in options" :command="item.lang">
-            {{ item.text }}
-        </el-dropdown-item>
+                {{ item.text }}
+            </el-dropdown-item>
         </template>
     </el-dropdown>
 </template>
 
 <script lang="ts" setup name="LanguagePicker">
-
-import { onBeforeMount } from 'vue';
-import i18n from '@/i18n';
-import { useI18n } from 'vue-i18n';
+import { onBeforeMount } from "vue";
+import i18n from "@/i18n";
+import { useI18n } from "vue-i18n";
 const t = useI18n();
 
-import { useLocalStore } from '@/store';
+import { useLocalStore } from "@/store";
 const local = useLocalStore();
 
 import logo_lang from "@/assets/language.svg";
 
 const options = [
-    { lang: 'dev', text: 'dev' },
-    { lang: 'zh-cn', text: '简体中文' },
-    { lang: 'de', text: 'name'},
-    { lang: 'en', text: 'English'}
-]
+    { lang: "dev", text: "dev" },
+    { lang: "zh-cn", text: "简体中文" },
+    { lang: "de", text: "name" },
+    { lang: "en", text: "English" },
+];
 
 onBeforeMount(() => {
-    i18n.global.locale.value=local.language
-})
+    i18n.global.locale.value = local.language;
+});
 
 const changeLanguage = (value: any) => {
-    i18n.global.locale.value = value
-    local.language = value
-}
-
+    i18n.global.locale.value = value;
+    local.language = value;
+};
 </script>
