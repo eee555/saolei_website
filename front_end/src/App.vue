a532fda085ab71f5a3d90898b80a99db5d7600b1
Tianyi Pu <912396513@qq.com>
2024-06-18 08:40:46 +0100

    格式化App.vue

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -49,21 +49,17 @@
             <Login @login="user_login" @logout="user_logout"></Login>
         </div>
     </el-menu>
-
-
     <div class="common-layout">
         <el-container>
-            <div class="header_all" style="padding-top: 0;overflow: hidden;">
-                <div class="content" style="padding-top: 16px;">
+            <div class="header_all" style="padding-top: 0; overflow: hidden">
+                <div class="content" style="padding-top: 16px">
                     <router-view />
                 </div>
             </div>
-
-
-            <el-footer style="margin: auto;">
+            <el-footer style="margin: auto">
                 Copyright @ 2023　<a href="http://fff666.top">元扫雷网 fff666.top</a>　版权所有　<a
                     href="https://beian.miit.gov.cn/">苏ICP备2023056839号-1</a>
-                <span style="width:12px; display:inline-block"></span>
+                <span style="width: 12px; display: inline-block"></span>
                 <a href="https://beian.mps.gov.cn/#/query/webSearch?code=32020602001691" rel="noreferrer"
                     target="_blank">苏公网安备32020602001691</a>
             </el-footer>
@@ -76,7 +72,7 @@
         <template #footer>
             <span class="dialog-footer">
                 <el-checkbox v-model="never_show_notice">不再显示此对话框</el-checkbox>
-                <el-button type="primary" @click="handle_notice_close();">
+                <el-button type="primary" @click="handle_notice_close()">
                     确认
                 </el-button>
             </span>
@@ -84,31 +80,33 @@
     </el-dialog>
 </template>
 
-<script setup lang='ts'>
-import { ref, reactive, onMounted, computed } from 'vue'
+<script setup lang="ts">
+import { ref, reactive, onMounted, computed } from "vue";
 import Menu from "./components/Menu.vue";
-import LanguagePicker from './components/LanguagePicker.vue';
+import LanguagePicker from "./components/LanguagePicker.vue";
+import Login from "./components/Login.vue";
 // import { LoginStatus } from "@/utils/common/structInterface"
 import useCurrentInstance from "@/utils/common/useCurrentInstance";
-import { useUserStore } from './store'
-const store = useUserStore()
+import { useLocalStore, useUserStore } from "./store";
+const store = useUserStore();
+const local = useLocalStore();
 
-import { useI18n } from 'vue-i18n';
+import { useI18n } from "vue-i18n";
 const t = useI18n();
 
 const { proxy } = useCurrentInstance();
-import logo_1 from '@/assets/logo.png';
-import logo_2 from '@/assets/logo2.png';
+import logo_1 from "@/assets/logo.png";
+import logo_2 from "@/assets/logo2.png";
 
-import { useRouter } from 'vue-router'
-const router = useRouter()
+import { useRouter } from "vue-router";
+const router = useRouter();
 
 // const player_visible = ref(false)
-const notice_visible = ref(false)
-const never_show_notice = ref(false)
+const notice_visible = ref(false);
+const never_show_notice = ref(false);
 
 // 主要是切换后的高亮
-const menu_index = ref()
+const menu_index = ref();
 
 const menu_items = [
     { index: "ranking", icon: "Trophy", content: "menu.ranking" },
@@ -121,8 +119,7 @@ const menu_items = [
 const notice = ref(`
 1、即日起开始删档公测，公测与开发同步进行。公测结束后，在正式上线之前会删除所有数据。
 2、相关意见、问题和建议请移步至此处[https://gitee.com/ee55/saolei_website/issues]发表。
-`)
-
+`);
 
 onMounted(() => {
     const notice_hash = localStorage.getItem("notice") as String;
@@ -130,32 +127,31 @@ onMounted(() => {
         notice_visible.value = true;
     }
 
-
     console.log(`
   元扫雷网(fff666.top)开发团队，期待您的加入: 2234208506@qq.com
   `);
-    router.isReady().then(() => { menu_index.value = router.currentRoute.value.fullPath; })
-
-
-})
+    router.isReady().then(() => {
+        menu_index.value = router.currentRoute.value.fullPath;
+    });
+});
 
 const player_url = computed(() => {
-    return '/player/' + store.user.id;
-})
+    return "/player/" + store.user.id;
+});
 
 const user_login = () => {
     // player_visible.value = true;
     // tab_width.value = "14vw";
-}
+};
 
 const user_logout = () => {
     // console.log(router.currentRoute.value.fullPath);
     // 如果切在我的地盘，就切到主页
-    if (router.currentRoute.value.fullPath.slice(0, 7) == '/player') {
+    if (router.currentRoute.value.fullPath.slice(0, 7) == "/player") {
         menu_index.value = "/";
         proxy.$router.push("/");
     }
-}
+};
 
 // const goback_home = () => {
 //     router.push("/")
@@ -167,31 +163,27 @@ const handle_notice_close = () => {
         localStorage.setItem("notice", hash_code(notice.value) + "");
     }
     notice_visible.value = false;
-}
+};
 
 const hash_code = function (t: string) {
-    var hash = 0, i, chr;
+    var hash = 0,
+        i,
+        chr;
     if (t.length === 0) return hash;
     for (i = 0; i < t.length; i++) {
         chr = t.charCodeAt(i);
-        hash = ((hash << 5) - hash) + chr;
+        hash = (hash << 5) - hash + chr;
         hash |= 0; // 32bit integer
     }
     return hash;
 };
-
-
-
-
 </script>
 
-
-<style lang='less'>
+<style lang="less">
 body {
     overflow-y: scroll;
     margin: 0;
 }
-
 
 .logo:hover {
     cursor: pointer;
