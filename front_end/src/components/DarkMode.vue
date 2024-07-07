de9fe9fae94284915cfbe6ae4a966c8dc5a8c4f0
Tianyi Pu <912396513@qq.com>
2024-07-07 20:35:11 +0100

    颜色模式改为深、浅、自动三种

---
+++ front_end/src/components/DarkMode.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-radio-group v-model="store" size="small" style="vertical-align: middle;">
+        <el-radio-button :label="t.t('setting.colorscheme.light')" value="light" />
+        <el-radio-button :label="t.t('setting.colorscheme.auto')" value="auto" />
+        <el-radio-button :label="t.t('setting.colorscheme.dark')" value="dark" />
+    </el-radio-group>
+</template>
+
+<script lang="ts" setup name="DarkMode">
+
+import { useI18n } from 'vue-i18n';
+import { useColorMode } from '@vueuse/core';
+
+const t = useI18n()
+const { system, store } = useColorMode()
+
+</script>
