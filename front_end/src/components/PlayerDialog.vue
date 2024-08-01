af0306b5b7e1cfbb5692aad8cb6b73ce9b5c3656
Tianyi Pu <912396513@qq.com>
2024-08-01 14:10:53 +0100

    录像播放器定宽

--- front_end/src/components/PlayerDialog.vue
+++ front_end/src/components/PlayerDialog.vue
@@ -1,6 +1,6 @@
 <template>
     <el-dialog v-model="videoplayerstore.visible" style="backdrop-filter: blur(1px);" draggable align-center destroy-on-close
-        :modal="false" :lock-scroll="false">
+        :modal="false" :lock-scroll="false" width="700">
         <iframe class="flop-player-iframe flop-player-display-none" style="width: 100%; height: 500px; border: 0px"
             src="/flop/index.html" ref="video_iframe"></iframe>
     </el-dialog>
