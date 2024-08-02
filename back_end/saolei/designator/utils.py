6bfed88df158da6a0065d1c992a36d87c42eb220
Tianyi Pu <912396513@qq.com>
2024-08-02 17:44:15 +0100

    Create utils.py

---
+++ back_end/saolei/designator/utils.py
@@ -0,0 +1,11 @@
+from userprofile.models import UserProfile
+
+# 增加标识后扫描所有该标识的录像，返回操作的录像数量
+def add_designator_aftermath(user: UserProfile, designator: str):
+    # TODO
+    return 0
+
+# 删除标识后扫描所有该标识的录像，返回操作的录像数量
+def del_designator_aftermath(user: UserProfile, designator: str):
+    # TODO
+    return 0
