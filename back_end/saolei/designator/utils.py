77546a56585815a4ca8b674be60916d6750cdfaf
Tianyi Pu <912396513@qq.com>
2024-08-05 13:04:00 +0100

    新增`update_state`方法

--- back_end/saolei/designator/utils.py
+++ back_end/saolei/designator/utils.py
@@ -1,7 +1,7 @@
 from userprofile.models import UserProfile
 from videomanager.models import VideoModel
 from videomanager.views import approve_single
-from videomanager.view_utils import freeze_single, update_personal_record_stock
+from videomanager.view_utils import update_personal_record_stock, update_state
 
 # 增加标识后扫描所有该标识的录像，返回操作的录像数量
 def add_designator_aftermath(user: UserProfile, designator: str):
@@ -9,6 +9,7 @@ def add_designator_aftermath(user: UserProfile, designator: str):
     for video in video_list:
         if video.state == VideoModel.State.DESIGNATOR:
             approve_single(video, False)
+            update_state(video, VideoModel.State.OFFICIAL)
     return len(video_list)
 
 # 删除标识后扫描所有该标识的录像，返回操作的录像数量
@@ -17,5 +18,6 @@ def del_designator_aftermath(user: UserProfile, designator: str):
     for video in video_list:
         if video.state == VideoModel.State.OFFICIAL:
             freeze_single(video, state=VideoModel.State.DESIGNATOR, update_ranking=False)
+            update_state(video, VideoModel.State.DESIGNATOR, update_ranking=False)
     update_personal_record_stock(user)
     return len(video_list)
