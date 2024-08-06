98a81350cb241a880af848674fc24fed934f98bb
Tianyi Pu <912396513@qq.com>
2024-08-06 10:33:17 +0100

    重写`add_designator`

--- back_end/saolei/designator/utils.py
+++ back_end/saolei/designator/utils.py
@@ -14,14 +14,6 @@ def verify_designator(designator: str):
     new_designator = Designator.objects.create(designator=designator, safe=verify_text(designator))
     return new_designator.safe
 
-# 增加标识后扫描所有该标识的录像，返回操作的录像数量
-def add_designator_aftermath(user: UserProfile, designator: str):
-    video_list = VideoModel.objects.filter(player=user, video__designator=designator)
-    for video in video_list:
-        if video.state == VideoModel.State.DESIGNATOR:
-            update_state(video, VideoModel.State.OFFICIAL)
-    return len(video_list)
-
 # 删除标识后扫描所有该标识的录像，返回操作的录像数量
 def del_designator_aftermath(user: UserProfile, designator: str):
     video_list = VideoModel.objects.filter(player=user, video__designator=designator)
