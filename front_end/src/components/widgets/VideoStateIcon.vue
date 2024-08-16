81f515f121a55a8ef2f49bf81bc7df67d8d0d343
Tianyi Pu <912396513@qq.com>
2024-08-16 23:47:43 +0100

    add VideoStateIcon

---
+++ front_end/src/components/widgets/VideoStateIcon.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-text v-if="state == 'd'" type="warning">
+        <Warning />
+    </el-text>
+    <el-text v-else-if="state == 'c'" type="success">
+        <CircleCheck />
+    </el-text>
+    <el-icon v-else type="error">
+        <QuestionFilled />
+    </el-icon>
+</template>
+
+<script setup lang="ts">
+const data = defineProps({
+    state: {
+        type: String,
+        default: "z",
+    },
+});
+</script>
