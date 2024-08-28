8902e2c5842a065d5fe3aceadaa2e43f5d006e3a
eee555 <2234208506@qq.com>
2024-08-28 10:04:48 +0800

    fix：文章名不以[开头时出错。添加删新闻定时任务

--- back_end/saolei/videomanager/management/commands/runapschedulervideomanager.py
+++ back_end/saolei/videomanager/management/commands/runapschedulervideomanager.py
@@ -21,22 +21,23 @@ logger = logging.getLogger(__name__)
 # 定时任务文档
 # https://pypi.org/project/django-apscheduler/
 
+def n_days_ago(obj, n=7) -> bool:
+    d = datetime.strptime(obj['time'], "%Y-%m-%dT%H:%M:%SZ")
+    return (timezone.now().replace(tzinfo=None) - d).days > n
 
 # 定时清除最新录像，直至剩下最近7天的或剩下不到100条
-# 可能有时区问题
 def delete_newest_queue():
     if cache.hlen("newest_queue") <= 100:
         return
     newest_queue_ids = cache.hgetall("newest_queue")
     # newest_queue_ids的示例：
-    # {b'1': b'{"time": "2024-05-22 17:31:06", "player": "\\u5b9e\\u540d", "player_id": 1,
+    # {b'1': b'{"time": "2024-05-22T17:31:06Z", "player": "\\u5b9e\\u540d", "player_id": 1,
     #  "level": "b", "mode": "00", "timems": 4770, "bv": 23, "bvs": 4.821802935010482}', 
-    # b'4': b'{"time": "2024-05-22 17:31:09", "player": "\\u5b9e\\u540d", "player_id": 1, 
+    # b'4': b'{"time": "2024-05-22T17:31:09Z", "player": "\\u5b9e\\u540d", "player_id": 1, 
     # "level": "e", "mode": "00", "timems": 74710, "bv": 227, "bvs": 3.0384152054611167}'}
     for key in newest_queue_ids.keys():
-        a = json.loads(newest_queue_ids[key])['time']
-        d = datetime.strptime(a, "%Y-%m-%d %H:%M:%S")
-        if (timezone.now().replace(tzinfo=None) - d).days > 7:
+        a = json.loads(newest_queue_ids[key])
+        if n_days_ago(a):
             cache.hdel("newest_queue", key)
 
 
@@ -45,7 +46,22 @@ def delete_freezed_video():
     ddl = timezone.now() - timezone.timedelta(days=7)
     VideoModel.objects.filter(upload_time__lt=ddl, state="b").delete()
 
-
+# 定时清除新闻
+def delete_news_queue():
+    if cache.llen("news_queue") <= 100:
+        return
+    news_queue_list = cache.lrange("news_queue", 0, -1)
+    # news_queue的示例：
+    # [b'{"time": "2024-08-09T17:58:06Z", "player": "\\u7530\\u94ed\\u626c",
+    #  "player_id": 5, "video_id": 6, "index": "timems", "mode": "std", 
+    # "level": "e", "value": "54.760", "delta": "-8.130"}', b'{"time": 
+    # "2024-08-09T17:48:38Z", "player": "\\u7530\\u94ed\\u626c", "player_id":
+    #  5, "video_id": 5, "index": "timems", "mode": "nf", "level": "e", "value":
+    #  "65.370", "delta": "\\u65b0"}']
+    news_queue_list = [x for x in news_queue_list if not n_days_ago(json.loads(x))]
+    cache.delete("news_queue")
+    if news_queue_list:
+        cache.rpush("news_queue", *news_queue_list)
 
 
 # The `close_old_connections` decorator ensures that database connections, that have become
@@ -96,6 +112,18 @@ class Command(BaseCommand):
             )
         logger.info("Added job 'delete_freezed_video'.")
 
+
+        scheduler.add_job(
+            delete_news_queue,
+            trigger=CronTrigger(
+                day_of_week="*", hour="17", minute="03"
+                ),
+            id="delete_news_queue",
+            misfire_grace_time=30,
+            max_instances=1,
+            replace_existing=True,
+            )
+        logger.info("Added job 'delete_news_queue'.")
 
 
         scheduler.add_job(
