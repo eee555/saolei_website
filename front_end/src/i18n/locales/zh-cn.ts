5efb04845ed2f9dd00de884cb0bb830024cda7f9
eee555 <2234208506@qq.com>
2024-09-04 23:02:04 +0800

    升级ms_toollib、升级元扫雷下载链接

--- front_end/src/i18n/locales/zh-cn.ts
+++ front_end/src/i18n/locales/zh-cn.ts
@@ -80,7 +80,8 @@ export const zhCn = {
             software_download: '软件下载',
             download_link: '下载链接',
             metasweeper: '元扫雷',
-            metasweeper_int: '（开源扫雷网官方扫雷软件，包含八种模式的无猜扫雷、第三代录像播放器、自带光学识别求解器。）', // introduction
+            metasweeper_int: '（开源扫雷网官方扫雷软件，开源扫雷网支持其标准、竞速（标准）无猜模式。）', // introduction
+            metasweeper_int2: '（开源扫雷网官方扫雷软件，开源扫雷网支持其标准、标准无猜、强无猜、弱可猜模式。）', // introduction
             arbiter_int: '（同时受到国际网、中国扫雷网、开源扫雷网认可的老牌扫雷软件，只有标准模式。）',
         },
         state: {
