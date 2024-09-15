5e936210445153ff73bbb02ce97beba2c6d29e05
eee555 <2234208506@qq.com>
2024-09-16 01:17:44 +0800

    打包流程

--- back_end/saolei/start.sh
+++ back_end/saolei/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -r ../../font_end
-python3 manage.py collectstatic --noinput
+rm -rf ../../font_end
+# python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runapschedulermonitor
