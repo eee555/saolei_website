7871d4c2dfd5783a5e57f3348ffb59de42053ac6
eee555 <2234208506@qq.com>
2024-09-16 02:00:24 +0800

    部署流程

--- back_end/saolei/start.sh
+++ back_end/saolei/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf ../../font_end
-# python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runapschedulermonitor
