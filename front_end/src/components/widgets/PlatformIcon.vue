7aa56a9475ede1a316efe4076c55a5c1628dab8d
Tianyi Pu <912396513@qq.com>
2024-09-25 08:52:05 +0100

    账号绑定 国际网和wom的引导

--- front_end/src/components/widgets/PlatformIcon.vue
+++ front_end/src/components/widgets/PlatformIcon.vue
@@ -1,25 +1,12 @@
 <template>
-    <el-text v-if="platform=='a'">
-        Authoritative Minesweeper
-    </el-text>
-    <el-link v-else-if="platform=='c'" href="http://saolei.wang/" target="_blank" style="vertical-align: bottom;">
-        扫雷网
+    <el-link :href="platformlist[platform].url" target="_blank" style="vertical-align: bottom;">
+        {{ platformlist[platform].name }}
     </el-link>
-    <el-text v-else-if="platform=='g'">
-        Minesweeper GO
-    </el-text>
-    <el-text v-else-if="platform=='l'">
-        扫雷联萌
-    </el-text>
-    <el-text v-else-if="platform=='s'">
-        Scoreganizer
-    </el-text>
-    <el-text v-else-if="platform=='w'">
-        Minesweeper Online
-    </el-text>
 </template>
 
 <script setup lang="ts">
+import { platformlist } from '@/utils/common/accountLinkPlatforms';
+
 const data = defineProps({
     platform: {
         type: String,
