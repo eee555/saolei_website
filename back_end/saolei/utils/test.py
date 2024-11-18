7b1397c036534c7bf612684a492d38379b9b81ff
LjingZhijoin Group <107012527+ljzloser@users.noreply.github.com>
2024-11-19 00:01:46 +0800

    修改bug (#163)
    * 增加wom爬取视频信息。
    
    * 增加websocket-client
    
    * 增加几个sleep() 防止空循环，拉高cpu
    
    * sleep时间修改为0.1
    
    * 增加了一个异常的回调处理。内部不处理任何异常，只会在异常之后等待一段重新发起请求,并将出现异常的id重新加入请求队列。

--- back_end/saolei/utils/test.py
+++ back_end/saolei/utils/test.py
@@ -10,7 +10,7 @@ class TestWOM(unittest.TestCase):
     def test_insert_video_id(self):
         self.count = 0
         # 创建 WOM 实例并传入 mock_callback
-        wom = WOM(callback=self.callbackFunc, timeout=10, reConnectTime=60)
+        wom = WOM(videoInfoFunc=self.callbackFunc, disConnectTime=10, raiseReConnectTime=60,errorFunc=self.errorFunc)
         wom.start()  # 启动 WOM
 
         wom.insertVideoId('3071736950')
@@ -48,6 +48,8 @@ class TestWOM(unittest.TestCase):
                              "Insert video Info 214")
         self.count += 1
 
+    def errorFunc(self, e:Exception):
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
     unittest.main()
