80d6c337b2daae9f65424e93a900db37de1deecf
Tianyi Pu <912396513@qq.com>
2024-12-19 11:16:13 +0000

    Update VideoStateFilter.vue

--- front_end/src/components/Filters/VideoStateFilter.vue
+++ front_end/src/components/Filters/VideoStateFilter.vue
@@ -1,7 +1,7 @@
 <template>
     <el-checkbox-group v-model="checkboxGroup" size="small" style="display:inline-block">
         <el-checkbox-button v-for="item of statelist" :value="item.state">
-            {{ $t('common.state.' + item.state) }}
+            {{ t('common.state.' + item.state) }}
         </el-checkbox-button>
     </el-checkbox-group>
 </template>
@@ -18,6 +18,6 @@ const statelist = [
     {state: 'b'},
 ];
 
-const t = useI18n();
+const { t } = useI18n();
 
 </script>
