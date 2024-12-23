9343a2d537a23df06dd3cdfe5f19791c7c6d1d47
Tianyi Pu <912396513@qq.com>
2024-12-23 23:12:10 +0000

    fix: #170 遗留问题

--- back_end/saolei/identifier/views.py
+++ back_end/saolei/identifier/views.py
@@ -8,6 +8,7 @@ from utils.response import HttpResponseConflict
 from videomanager.models import VideoModel
 from videomanager.view_utils import update_state, update_personal_record_stock
 from userprofile.decorators import login_required_error
+from config.text_choices import MS_TextChoices
 
 # 请求修改自己的标识
 @require_POST
@@ -28,8 +29,8 @@ def add_identifier(request):
     if not video_list:
         return JsonResponse({'type': 'error', 'object': 'identifier', 'category': 'notFound'})
     for video in video_list:
-        if video.state == VideoModel.State.IDENTIFIER:
-            update_state(video, VideoModel.State.OFFICIAL)
+        if video.state == MS_TextChoices.State.IDENTIFIER:
+            update_state(video, MS_TextChoices.State.OFFICIAL)
     identifier.userms = user.userms
     identifier.save()
     user.userms.identifiers.append(identifier_text)
@@ -55,8 +56,8 @@ def del_identifier(request):
     logger.info(f'用户 {user.username}#{user.id} 解绑标识 "{identifier_text}"')
     video_list = VideoModel.objects.filter(player=user, video__identifier=identifier_text)
     for video in video_list:
-        if video.state == VideoModel.State.OFFICIAL:
-            update_state(video, VideoModel.State.IDENTIFIER, update_ranking=False)
+        if video.state == MS_TextChoices.State.OFFICIAL:
+            update_state(video, MS_TextChoices.State.IDENTIFIER, update_ranking=False)
     if video_list:
         update_personal_record_stock(user)
     return JsonResponse({'type': 'success', 'object': 'identifier', 'category': 'add', 'value': len(video_list)})
