80296fea6a8ceeea3985869781b3bb8480d21fad
eee555 <2234208506@qq.com>
2025-01-08 17:53:19 +0800

    Update start.sh

--- back_end/saolei/start.sh
+++ back_end/saolei/start.sh
@@ -4,7 +4,6 @@ rm -rf /root/saolei/static
 # 搜集静态文件到/root/saolei/static，按照setting的配置，只有dist
 python3 manage.py collectstatic --noinput
 # 前端文件的打包结果来自github工作流
-rm -r dist
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runapschedulermonitor
