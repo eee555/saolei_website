fa6dab988901ce259ca96629432815ce867ee08b
Tianyi Pu <912396513@qq.com>
2025-01-20 14:40:09 +0000

    格式化

--- back_end/saolei/userprofile/views.py
+++ back_end/saolei/userprofile/views.py
@@ -1,20 +1,20 @@
 import logging
-logger = logging.getLogger('userprofile')
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, JsonResponse, HttpResponseForbidden, HttpResponseNotFound, HttpResponseBadRequest
 from .forms import UserLoginForm, UserRegisterForm, UserRetrieveForm, EmailForm
 from captcha.models import CaptchaStore
 import json
 import os
-from .models import EmailVerifyRecord,UserProfile
+from .models import EmailVerifyRecord, UserProfile
 from utils import send_email
 from msuser.models import UserMS
 from django_ratelimit.decorators import ratelimit
 from django.views.decorators.http import require_GET, require_POST
 from .decorators import staff_required
-from django.utils import timezone
 from config.flags import EMAIL_SKIP
 from .utils import judge_captcha, judge_email_verification, user_metadata
+
+logger = logging.getLogger('userprofile')
 
 # Create your views here.
 
@@ -44,7 +44,7 @@ def user_login(request):
         # 检测到小号
         logger.warning(f'{data["user_id"][:50]} is different from {str(user.id)}.')
     return JsonResponse({'type': 'success', 'user': user_metadata(user)})
-        
+
 
 @require_GET
 # 用cookie登录
@@ -52,6 +52,7 @@ def user_login_auto(request):
     if request.user.is_authenticated:
         return JsonResponse(user_metadata(request.user))
     return HttpResponse()
+
 
 def user_logout(request):
     logout(request)
@@ -71,7 +72,7 @@ def user_retrieve(request):
     if not judge_email_verification(email, email_captcha, emailHashkey):
         return JsonResponse({'type': 'error', 'object': 'emailcode'})
     if not (user := UserProfile.objects.filter(email=user_retrieve_form.cleaned_data['email']).first()):
-        return HttpResponseNotFound() # 前端已经查过重了，理论上不应该进到这里
+        return HttpResponseNotFound()  # 前端已经查过重了，理论上不应该进到这里
     # 设置密码(哈希)
     user.set_password(user_retrieve_form.cleaned_data['password'])
     user.save()
@@ -97,7 +98,7 @@ def user_register(request):
             # 设置密码(哈希)
             new_user.set_password(
                 user_register_form.cleaned_data['password'])
-            new_user.is_active = True # 自动激活
+            new_user.is_active = True  # 自动激活
             user_ms = UserMS.objects.create()
             new_user.userms = user_ms
             user_ms.save()
@@ -118,8 +119,8 @@ def user_register(request):
             errors = list(errors_dict.values())[0]
             return JsonResponse({'status': 105, 'msg': errors[0]["message"]})
         else:
-            return JsonResponse({'status': 101, 'msg': user_register_form.errors.\
-                                as_text().split("*")[-1]})
+            return JsonResponse({'status': 101, 'msg': user_register_form.errors.as_text().split("*")[-1]})
+
 
 @require_GET
 def check_collision(request):
@@ -133,6 +134,7 @@ def check_collision(request):
     if not user:
         return HttpResponse(False)
     return HttpResponse(True)
+
 
 # 【站长】任命解除管理员
 # http://127.0.0.1:8000/userprofile/set_staff/?id=1&is_staff=True
@@ -156,6 +158,7 @@ def set_staff(request):
     else:
         return HttpResponse("失败！is_staff需要为\"True\"或\"False\"（首字母大写）")
 
+
 # 【管理员】删除用户的个人信息，从服务器磁盘上完全删除，但不影响是否封禁
 # 站长可以删除管理员信息（如果站长也是管理员）。
 # http://127.0.0.1:8000/userprofile/del_user_info/?id=1
@@ -172,6 +175,7 @@ def del_user_info(request):
         os.remove(user.avatar.path)
     user.avatar = None
 
+
 # 创建验证码
 @ratelimit(key='ip', rate='60/h')
 def captcha(request):
@@ -182,6 +186,7 @@ def captcha(request):
     captcha = {'hashkey': hashkey}
     return captcha
 
+
 # 刷新验证码
 @ratelimit(key='ip', rate='60/h')
 def refresh_captcha(request):
@@ -189,52 +194,56 @@ def refresh_captcha(request):
     c.update({'status': 100})
     return HttpResponse(json.dumps(c), content_type='application/json')
 
+
 # 验证验证码，若通过，发送email
 @ratelimit(key='ip', rate='1/m')
 @require_POST
 def get_email_captcha(request):
     data = request.POST
     email_form = EmailForm(data=data)
-    if not email_form.is_valid(): # 正常工作的前端不应当发出的请求
+    if not email_form.is_valid():  # 正常工作的前端不应当发出的请求
         return HttpResponseBadRequest()
     capt = data.get("captcha")
     key = data.get("hashkey")
-    if not judge_captcha(capt, key): # 图形验证码不对
+    if not judge_captcha(capt, key):  # 图形验证码不对
         return JsonResponse({'type': 'error', 'object': 'captcha'})
     if EMAIL_SKIP:
         code, hashkey = send_email(data.get("email"), data.get("type"))
         return JsonResponse({'type': 'success', 'code': code, 'hashkey': hashkey})
-    if hashkey := send_email(data.get("email"), data.get("type")): # 邮件发送成功
+    if hashkey := send_email(data.get("email"), data.get("type")):  # 邮件发送成功
         return JsonResponse({'type': 'success', 'hashkey': hashkey})
-    else: # 邮件发送失败
+    else:  # 邮件发送失败
         return JsonResponse({'type': 'error', 'object': 'email'})
-        
-        
+
 
 # 管理员使用的操作接口，调用方式见前端的StaffView.vue
-get_userProfile_fields = ["id", "userms__identifiers", "userms__video_num_limit", "username", "first_name", "last_name", "email", "realname", "signature", "country", "left_realname_n", "left_avatar_n", "left_signature_n", "is_banned"] # 可获取的域列表
+get_userProfile_fields = ["id", "userms__identifiers", "userms__video_num_limit", "username", "first_name", "last_name", "email", "realname", "signature", "country", "left_realname_n", "left_avatar_n", "left_signature_n", "is_banned"]  # 可获取的域列表
+
+
 @require_GET
 @staff_required
 def get_userProfile(request):
     if userlist := UserProfile.objects.filter(id=request.GET["id"]).values(*get_userProfile_fields):
         return JsonResponse(userlist[0])
     return HttpResponseNotFound()
-    
+
 
 # 管理员使用的操作接口，调用方式见前端的StaffView.vue
-set_userProfile_fields = ["userms__identifiers", "userms__video_num_limit", "username", "first_name", "last_name", "email", "realname", "signature", "country", "left_realname_n", "left_avatar_n", "left_signature_n", "is_banned"] # 可修改的域列表
+set_userProfile_fields = ["userms__identifiers", "userms__video_num_limit", "username", "first_name", "last_name", "email", "realname", "signature", "country", "left_realname_n", "left_avatar_n", "left_signature_n", "is_banned"]  # 可修改的域列表
+
+
 @require_POST
 @staff_required
 def set_userProfile(request):
     userid = request.POST.get("id")
     user = UserProfile.objects.get(id=userid)
     if user.is_staff and user != request.user:
-        return HttpResponseForbidden() # 不能修改除自己以外管理员的信息
+        return HttpResponseForbidden()  # 不能修改除自己以外管理员的信息
     field = request.POST.get("field")
     if field not in set_userProfile_fields:
-        return HttpResponseForbidden() # 只能修改特定的域
+        return HttpResponseForbidden()  # 只能修改特定的域
     if field == "is_banned" and user.is_superuser:
-        return HttpResponseForbidden() # 站长不可被封禁
+        return HttpResponseForbidden()  # 站长不可被封禁
     value = request.POST.get("value")
     logger.warning(f'管理员 {request.user.username}#{request.user.id} 修改用户 {user.username}#{user.id} 域 {field} 从 {getattr(user, field)} 到 {value}')
     setattr(user, field, value)
