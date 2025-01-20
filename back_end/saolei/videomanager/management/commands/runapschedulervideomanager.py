0a74adebf79d2f56f8669588d914b6f19725fbb7
Tianyi Pu <912396513@qq.com>
2025-01-20 14:23:04 +0000

    格式化

--- back_end/saolei/videomanager/management/commands/runapschedulervideomanager.py
+++ back_end/saolei/videomanager/management/commands/runapschedulervideomanager.py
@@ -8,22 +8,22 @@ from django.core.management.base import BaseCommand
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 from django_apscheduler import util
-
 from django_redis import get_redis_connection
-cache = get_redis_connection("saolei_website")
 from django.utils import timezone
 from datetime import datetime
 import json
-from videomanager.models import VideoModel, ExpandVideoModel
+from videomanager.models import VideoModel
 
 logger = logging.getLogger(__name__)
+cache = get_redis_connection("saolei_website")
+
 
 # 定时任务文档
 # https://pypi.org/project/django-apscheduler/
-
 def n_days_ago(obj, n=7) -> bool:
     d = datetime.strptime(obj['time'], "%Y-%m-%dT%H:%M:%SZ")
     return (timezone.now().replace(tzinfo=None) - d).days > n
+
 
 # 定时清除最新录像，直至剩下最近7天的或剩下不到100条
 def delete_newest_queue():
@@ -32,8 +32,8 @@ def delete_newest_queue():
     newest_queue_ids = cache.hgetall("newest_queue")
     # newest_queue_ids的示例：
     # {b'1': b'{"time": "2024-05-22T17:31:06Z", "player": "\\u5b9e\\u540d", "player_id": 1,
-    #  "level": "b", "mode": "00", "timems": 4770, "bv": 23, "bvs": 4.821802935010482}', 
-    # b'4': b'{"time": "2024-05-22T17:31:09Z", "player": "\\u5b9e\\u540d", "player_id": 1, 
+    #  "level": "b", "mode": "00", "timems": 4770, "bv": 23, "bvs": 4.821802935010482}',
+    # b'4': b'{"time": "2024-05-22T17:31:09Z", "player": "\\u5b9e\\u540d", "player_id": 1,
     # "level": "e", "mode": "00", "timems": 74710, "bv": 227, "bvs": 3.0384152054611167}'}
     for key in newest_queue_ids.keys():
         a = json.loads(newest_queue_ids[key])
@@ -45,6 +45,7 @@ def delete_newest_queue():
 def delete_freezed_video():
     ddl = timezone.now() - timezone.timedelta(days=7)
     VideoModel.objects.filter(upload_time__lt=ddl, state="b").delete()
+
 
 # 定时清除新闻
 def delete_news_queue():
@@ -70,8 +71,8 @@ def delete_news_queue():
     # news_queue_list = cache.lrange("news_queue", 0, -1)
     # # news_queue的示例：
     # # [b'{"time": "2024-08-09T17:58:06Z", "player": "\\u7530\\u94ed\\u626c",
-    # #  "player_id": 5, "video_id": 6, "index": "timems", "mode": "std", 
-    # # "level": "e", "value": "54.760", "delta": "-8.130"}', b'{"time": 
+    # #  "player_id": 5, "video_id": 6, "index": "timems", "mode": "std",
+    # # "level": "e", "value": "54.760", "delta": "-8.130"}', b'{"time":
     # # "2024-08-09T17:48:38Z", "player": "\\u7530\\u94ed\\u626c", "player_id":
     # #  5, "video_id": 5, "index": "timems", "mode": "nf", "level": "e", "value":
     # #  "65.370", "delta": "\\u65b0"}']
@@ -83,14 +84,14 @@ def delete_news_queue():
 
 # The `close_old_connections` decorator ensures that database connections, that have become
 # unusable or are obsolete, are closed before and after your job has run. You should use it
-# to wrap any jobs that you schedule that access the Django database in any way. 
+# to wrap any jobs that you schedule that access the Django database in any way.
 @util.close_old_connections
 def delete_old_job_executions(max_age=604_800):
     """
     This job deletes APScheduler job execution entries older than `max_age` from the database.
     It helps to prevent the database from filling up with old historical records that are no
     longer useful.
-    
+
     :param max_age: The maximum length of time to retain historical job execution records.
                     Defaults to 7 days.
     """
@@ -108,53 +109,50 @@ class Command(BaseCommand):
             delete_newest_queue,
             trigger=CronTrigger(
                 day_of_week="*", hour="01", minute="08"
-                ),
+            ),
             id="delete_newest_queue",
             misfire_grace_time=30,
             max_instances=1,
             replace_existing=True,
-            )
+        )
         logger.info("Added job 'delete_newest_queue'.")
-
 
         scheduler.add_job(
             delete_freezed_video,
             trigger=CronTrigger(
                 day_of_week="*", hour="01", minute="13"
-                ),
+            ),
             id="delete_freezed_video",
             misfire_grace_time=30,
             max_instances=1,
             replace_existing=True,
-            )
+        )
         logger.info("Added job 'delete_freezed_video'.")
-
 
         scheduler.add_job(
             delete_news_queue,
             trigger=CronTrigger(
                 day_of_week="*", hour="17", minute="03"
-                ),
+            ),
             id="delete_news_queue",
             misfire_grace_time=30,
             max_instances=1,
             replace_existing=True,
-            )
+        )
         logger.info("Added job 'delete_news_queue'.")
-
 
         scheduler.add_job(
             delete_old_job_executions,
             trigger=CronTrigger(
                 day_of_week="mon", hour="00", minute="03"
-                ),  # Midnight on Monday, before start of the next work week.
+            ),  # Midnight on Monday, before start of the next work week.
             id="delete_old_job_executions",
             max_instances=1,
             replace_existing=True,
-            )
+        )
         logger.info(
             "Added weekly job: 'delete_old_job_executions'."
-            )
+        )
 
         try:
             logger.info("Starting scheduler...")
