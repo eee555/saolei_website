b8f4b508b01f7da3e0f34f678eb30c66840a2e42
Tianyi Pu <912396513@qq.com>
2025-01-22 09:19:52 +0000

    录像页、录像上传

--- back_end/saolei/videomanager/forms.py
+++ back_end/saolei/videomanager/forms.py
@@ -9,13 +9,8 @@ class UploadVideoForm(forms.ModelForm):
     # upload_time = forms.DateTimeField(auto_now_add=True, verbose_name="上传时间")
     review_code = forms.IntegerField(max_value=255, min_value=0, required=True)
     identifier = forms.CharField(max_length=MaxSizes.identifier, required=True)
-    cl_s = forms.FloatField(min_value=0.0, required=True)
     stnb = forms.FloatField(min_value=0.0, required=True)
     rqp = forms.FloatField(min_value=0.0, required=True)
-    ioe = forms.FloatField(min_value=0.0, required=True)
-    thrp = forms.FloatField(min_value=0.0, required=True)
-    corr = forms.FloatField(min_value=0.0, max_value=1.0, required=True)
-    ce_s = forms.FloatField(min_value=0.0, required=True)
 
     class Meta:
         model = VideoModel
