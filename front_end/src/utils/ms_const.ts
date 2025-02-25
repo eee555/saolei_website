f935da4da3d3fc8343bb34b8200f8e99284d0da9
Tianyi Pu <912396513@qq.com>
2025-02-25 14:12:55 +0800

    PB表支持STNB

--- front_end/src/utils/ms_const.ts
+++ front_end/src/utils/ms_const.ts
@@ -1,2 +1,10 @@
+import { useLocalStorage } from "@vueuse/core";
+
 export const MS_Levels = ['b', 'i', 'e'] as const;
 export type MS_Level = typeof MS_Levels[number];
+
+export const STNB_const = useLocalStorage('stnb_const',{
+    b: 47.299,
+    i: 153.73,
+    e: 435.001,
+})
