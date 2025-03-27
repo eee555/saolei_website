eca918978cae917d031eee690423cedb07807589
Tianyi Pu <912396513@qq.com>
2025-03-27 11:59:00 +0800

    refactor: LazyTooltip改成BaseTooltip

--- front_end/src/components/widgets/GameLevelIcon.vue
+++ front_end/src/components/widgets/GameLevelIcon.vue
@@ -1,13 +1,22 @@
 <template>
-    <LazyTooltip v-if="level == 'b'" content="8x8/10" :hide-after="0" :show-after="500">
+    <base-tooltip v-if="level == 'b'" :show-delay="500">
         <el-text v-t="'common.level.b'" />
-    </LazyTooltip>
-    <LazyTooltip v-else-if="level == 'i'" content="16x16/40" :hide-after="0" :show-after="500">
+        <template #content>
+            <el-text>8x8/10</el-text>
+        </template>
+    </base-tooltip>
+    <base-tooltip v-else-if="level == 'i'" :show-delay="500">
         <el-text v-t="'common.level.i'" />
-    </LazyTooltip>
-    <LazyTooltip v-else-if="level == 'e'" content="30x16/99" :hide-after="0" :show-after="500">
+        <template #content>
+            <el-text>16x16/40</el-text>
+        </template>
+    </base-tooltip>
+    <base-tooltip v-else-if="level == 'e'" :show-delay="500">
         <el-text v-t="'common.level.e'" />
-    </LazyTooltip>
+        <template #content>
+            <el-text>30x16/99</el-text>
+        </template>
+    </base-tooltip>
     <el-text v-else type="danger">
         <QuestionFilled style="width: 16px; height: 16px" />
     </el-text>
@@ -15,7 +24,7 @@
 
 <script setup lang="ts">
 import { ElText } from 'element-plus';
-import LazyTooltip from './LazyTooltip.vue';
+import BaseTooltip from '@/components/common/BaseTooltip.vue';
 
 defineProps({
     level: {
--- front_end/src/components/widgets/VideoStateIcon.vue
+++ front_end/src/components/widgets/VideoStateIcon.vue
@@ -1,14 +1,16 @@
 <template>
-    <el-text v-if="state == 'd'" type="warning">
-        <LazyTooltip :content="t('common.state.d')" :hide-after="0" :show-after="500">
-            <BaseIconIdentifier />
-        </LazyTooltip>
-    </el-text>
-    <el-text v-else-if="state == 'e'" type="warning">
-        <LazyTooltip :content="t('common.state.e')" :hide-after="0" :show-after="500">
-            <BaseIconExternal />
-        </LazyTooltip>
-    </el-text>
+    <base-tooltip v-if="state == 'd'" :show-delay="500">
+        <BaseIconIdentifier style="color: var(--el-color-warning)" />
+        <template #content>
+            <el-text v-t="'common.state.d'" />
+        </template>
+    </base-tooltip>
+    <base-tooltip v-else-if="state == 'e'" :show-delay="500">
+        <BaseIconExternal style="color: var(--el-color-warning)" />
+        <template #content>
+            <el-text v-t="'common.state.e'" />
+        </template>
+    </base-tooltip>
     <el-text v-else-if="state == 'c'" type="success">
         <base-icon-verified />
     </el-text>
@@ -21,10 +23,10 @@
 
 import { useI18n } from 'vue-i18n';
 import { ElText, ElIcon } from 'element-plus';
-import LazyTooltip from './LazyTooltip.vue';
 import BaseIconVerified from '@/components/common/BaseIconVerified.vue';
 import BaseIconIdentifier from '@/components/common/BaseIconIdentifier.vue';
 import BaseIconExternal from '@/components/common/BaseIconExternal.vue';
+import BaseTooltip from '@/components/common/BaseTooltip.vue';
 
 defineProps({
     state: {
