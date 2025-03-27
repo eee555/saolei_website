34510a49b0455493819bd8926dff02a2f6a3a09f
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2025-03-27 18:15:58 +0800

    refactor: 拆分SoftwareIcon (#276)

---
+++ front_end/src/components/common/BaseIconMetasweeper.vue
@@ -0,0 +1,15 @@
+<template>
+    <base-tooltip :show-delay="500">
+        <img style="width: 16px; height: 16px" src="@/assets/img/img_meta.png">
+        <template #content>
+            <el-text v-t="'common.software.metasweeper'" />
+        </template>
+    </base-tooltip>
+</template>
+
+<script setup lang="ts">
+
+import { ElText } from 'element-plus';
+import BaseTooltip from './BaseTooltip.vue';
+
+</script>
---
+++ front_end/src/components/common/BaseIconViennasweeper.vue
@@ -0,0 +1,15 @@
+<template>
+    <base-tooltip :show-delay="500">
+        <img style="width: 16px; height: 16px" src="@/assets/img/viennasweeper.ico">
+        <template #content>
+            <el-text>Viennasweeper</el-text>
+        </template>
+    </base-tooltip>
+</template>
+
+<script setup lang="ts">
+
+import { ElText } from 'element-plus';
+import BaseTooltip from './BaseTooltip.vue';
+
+</script>
