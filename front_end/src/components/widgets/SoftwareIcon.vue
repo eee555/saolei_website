11eafd6e6f2c9222af5759b177bc9140af3d7b3c
Tianyi Pu <912396513@qq.com>
2025-03-27 18:08:14 +0800

    refactor: 拆分SoftwareIcon

--- front_end/src/components/widgets/SoftwareIcon.vue
+++ front_end/src/components/widgets/SoftwareIcon.vue
@@ -1,28 +1,8 @@
 <template>
-    <base-tooltip v-if="software == 'a'" :show-delay="500">
-        <img style="width: 16px; height: 16px" src="../../assets/img/ms_arbiter_MAINICON.ico">
-        <template #content>
-            <el-text>Minesweeper Arbiter</el-text>
-        </template>
-    </base-tooltip>
-    <base-tooltip v-else-if="software == 'e'" :show-delay="500">
-        <img style="width: 16px; height: 16px" src="../../assets/img/img_meta.png">
-        <template #content>
-            <el-text v-t="'common.software.metasweeper'" />
-        </template>
-    </base-tooltip>
-    <base-tooltip v-else-if="software == 'm'" :show-delay="500">
-        <img style="width: 16px; height: 16px" src="../../assets/img/clone2007.ico">
-        <template #content>
-            <el-text>Clone 2007</el-text>
-        </template>
-    </base-tooltip>
-    <base-tooltip v-else-if="software == 'r'" :show-delay="500">
-        <img style="width: 16px; height: 16px" src="../../assets/img/viennasweeper.ico">
-        <template #content>
-            <el-text>Viennasweeper</el-text>
-        </template>
-    </base-tooltip>
+    <BaseIconArbiter v-if="software == 'a'" />
+    <BaseIconMetasweeper v-else-if="software == 'e'" />
+    <BaseIconViennasweeper v-else-if="software == 'r'" />
+    <BaseIconClone07 v-else-if="software == 'm'" />
     <el-text v-else type="danger">
         <QuestionFilled style="width: 16px; height: 16px" />
     </el-text>
@@ -30,7 +10,10 @@
 
 <script setup lang="ts">
 import { ElText } from 'element-plus';
-import BaseTooltip from '@/components/common/BaseTooltip.vue';
+import BaseIconArbiter from '@/components/common/BaseIconArbiter.vue';
+import BaseIconMetasweeper from '@/components/common/BaseIconMetasweeper.vue';
+import BaseIconViennasweeper from '../common/BaseIconViennasweeper.vue';
+import BaseIconClone07 from '../common/BaseIconClone07.vue';
 
 defineProps({
     software: {
