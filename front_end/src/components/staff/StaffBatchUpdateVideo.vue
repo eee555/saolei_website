edf853d4cdb1dc753830b195b7a0844d43b93a32
Tianyi Pu <912396513@qq.com>
2025-03-31 13:41:54 +0800

    codestyle

--- front_end/src/components/staff/StaffBatchUpdateVideo.vue
+++ front_end/src/components/staff/StaffBatchUpdateVideo.vue
@@ -1,12 +1,18 @@
 <template>
-    批量刷新录像<br />
+    批量刷新录像<br>
     开始ID<el-input-number v-model="startid" />&nbsp;
     结束ID<el-input-number v-model="endid" />&nbsp;
     批处理数量<el-input-number v-model="batchsize" />&nbsp;
-    <el-button :disabled="working" @click="startBatchUpdate">开始！</el-button>
-    <el-button :disabled="!working" @click="stopBatchUpdate">停止！</el-button><br />
+    <el-button :disabled="working" @click="startBatchUpdate">
+        开始！
+    </el-button>
+    <el-button :disabled="!working" @click="stopBatchUpdate">
+        停止！
+    </el-button><br>
     客户端会将需要处理的ID段按照批处理数量发送到服务器进行批处理。批处理数量越大，服务器处理效率越高，但是如果批处理数量过大，会导致连接超时。
-    <el-text v-for="(log, index) in logList" :key="index" :style="{ display: 'block' }">{{ log }}</el-text>
+    <el-text v-for="(log, index) in logList" :key="index" :style="{ display: 'block' }">
+        {{ log }}
+    </el-text>
 </template>
 
 <script setup lang="ts">
@@ -35,7 +41,7 @@ async function startBatchUpdate() {
         await proxy.$axios.post('video/update/batch/', { startid: start, endid: end }).then(
             function (response) {
                 logList.value.push(`${start}至${end}已处理完成，成功${response.data.successCount}个，失败${response.data.errorList.length}个`);
-                if (response.data.errorList.length > 0){
+                if (response.data.errorList.length > 0) {
                     logList.value.push(`失败的录像为：${response.data.errorList.join('、')}`);
                 }
             },
