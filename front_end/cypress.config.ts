1e4bd71a8794565c5aab3216d37425fcdf6e204f
Tianyi Pu <912396513@qq.com>
2025-04-26 09:25:19 +0800

    Update cypress.config.ts

--- front_end/cypress.config.ts
+++ front_end/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:8000',
+        baseUrl: 'http://localhost:8080',
         setupNodeEvents(_on, _config) {
             // implement node event listeners here
         },
