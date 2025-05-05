fe01261c877f86082b0ec5712c8aed2b12973004
Tianyi Pu <912396513@qq.com>
2025-05-05 14:35:29 +0800

    添加测试

---
+++ front_end/src/components/visualization/BBBvSummary/App.cy.ts
@@ -0,0 +1,37 @@
+import BBBvSummary from './App.vue';
+import i18n from '@/i18n';
+import { pinia } from '@/store/create';
+import { VideoAbstract } from '@/utils/videoabstract';
+
+describe('<BBBvSummary />', () => {
+    before(() => {
+        cy.fixture('videoAbstractList.json').then((data) => {
+            cy.log(data.data);
+            Cypress.env('videoList', data.data.map((video) => new VideoAbstract(video)));
+        });
+        cy.clock(new Date('2025-12-15T00:00:00Z'));
+    });
+    it('renders', () => {
+        // see: https://on.cypress.io/mounting-vue
+        const videoList = Cypress.env('videoList');
+        cy.mount(BBBvSummary, {
+            props: {
+                level: 'e',
+                videoList: videoList,
+            },
+            global: {
+                plugins: [i18n, pinia],
+            },
+        });
+        cy.get('[data-cy=summary]').contains('Expert 2 Bv in total');
+        cy.get('[data-cy=bv-100]').contains('30.000');
+        cy.get('[data-cy=bv-155]').then(($el) => {
+            const styles = $el[0].style;
+            const variables = Array.from(styles).filter((prop) => prop.startsWith('--') && prop.endsWith('fontColor'));
+            variables.forEach((varName) => {
+                const value = styles.getPropertyValue(varName);
+                expect(value).to.equal('#606266');
+            });
+        });
+    });
+});
