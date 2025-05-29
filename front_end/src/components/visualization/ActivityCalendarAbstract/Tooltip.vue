9e6ceec135a81fa6ea1a20f6a1b2d70e30e50e2f
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2025-05-29 18:09:25 +0800

    日历图性能优化 (#329)
    * 日历图性能优化
    
    共享一个Tooltip，删除Cell抽象
    
    * Update App.cy.ts
    
    * add cypress-real-events
    
    * Update package.json

---
+++ front_end/src/components/visualization/ActivityCalendarAbstract/Tooltip.vue
@@ -0,0 +1,50 @@
+<template>
+    <el-text v-if="videos.length == 0">
+        {{ t('activityCalendar.tooltip.noVideoOnDate', [toISODateString(date)]) }}
+    </el-text>
+    <template v-else>
+        <el-text>
+            {{ t('activityCalendar.tooltip.uploadedNVideosOnDate', [toISODateString(date), videos.length]) }}
+        </el-text>
+        <br>
+        <span v-for="i in count.b" :key="i" class="dot" style="background-color: #f00;" />
+        <span v-for="i in count.i" :key="i" class="dot" style="background-color: #080;" />
+        <span v-for="i in count.e" :key="i" class="dot" style="background-color: #00f;" />
+    </template>
+</template>
+
+<script setup lang="ts">
+import { ElText } from 'element-plus';
+import { toISODateString } from '@/utils/datetime';
+import { VideoAbstract } from '@/utils/videoabstract';
+import { ref, watch } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+const props = defineProps({
+    date: { type: Date, required: true },
+    videos: { type: Array<VideoAbstract>, default: () => [] },
+});
+
+const count = ref({ b: 0, i: 0, e: 0 });
+
+watch(() => props.videos, () => {
+    count.value.b = 0;
+    count.value.i = 0;
+    count.value.e = 0;
+    for (const video of props.videos) {
+        count.value[video.level]++;
+    }
+}, { immediate: true });
+
+</script>
+
+<style lang="less" scoped>
+.dot {
+    height: 12px;
+    width: 12px;
+    border-radius: 50%;
+    display: inline-block;
+}
+</style>
