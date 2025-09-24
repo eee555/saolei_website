fd6aa46356253cac7ddf494909919909039800d9
Tianyi Pu <912396513@qq.com>
2025-09-24 14:52:22 +0800

    Create tournaments.py

---
+++ back_end/saolei/config/tournaments.py
@@ -0,0 +1,15 @@
+
+class GSC_Defaults:
+    BT = 10000
+    IT = 60000
+    ET = 240000
+
+    B_BV_MIN = 10
+    I_BV_MIN = 30
+    E_BV_MIN = 100
+
+    HOST_ID = 48  # 郭锦洋
+
+
+class TournamentWeights:
+    GSC = 1000
