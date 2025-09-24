e0cb821b6c51890673aaa379760b9d6bb69fa70a
Tianyi Pu <912396513@qq.com>
2025-09-24 14:58:26 +0800

    Update views.py

--- back_end/saolei/tournaments/views.py
+++ back_end/saolei/tournaments/views.py
@@ -1,13 +1,16 @@
 
 from django.views.decorators.http import require_GET, require_POST
-from django.http import HttpResponseBadRequest, JsonResponse, HttpResponse, HttpResponseNotFound, HttpResponseForbidden
+from django.http import HttpResponseBadRequest, JsonResponse, HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpRequest
 from userprofile.decorators import banned_blocked, staff_required, login_required_error
-from .models import Tournament, TournamentParticipant
+from .models import Tournament, TournamentParticipant, GSCTournament
 from .forms import TournamentForm
 from .utils import refresh_tournament
 from utils import verify_text
 from config.text_choices import Tournament_TextChoices
 from datetime import datetime
+from config.tournaments import GSC_Defaults, TournamentWeights
+from utils.response import HttpResponseConflict
+from userprofile.models import UserProfile
 
 @require_POST
 @banned_blocked
@@ -111,3 +114,95 @@ def tournament_checkin(request):
 
 def download_tournament(request):
     pass
+
+
+@require_POST
+def new_GSC_tournament(request: HttpRequest):
+    if request.user.id != GSC_Defaults.HOST_ID:
+        return HttpResponseForbidden()
+    start_time = request.POST.get('start_time')
+    end_time = request.POST.get('end_time')
+    
+    if not start_time or not end_time:
+        status = Tournament_TextChoices.State.PENDING
+    elif datetime.now() < start_time:
+        status = Tournament_TextChoices.State.PREPARING
+    else:
+        return JsonResponse({'type': 'error', 'msg': 'invalid_start_time'})
+    
+    order = request.POST.get('order')
+    if GSCTournament.objects.filter(order=order).exists():
+        return HttpResponseConflict()
+    
+    new_GSC = GSCTournament.objects.create(
+        start_time=start_time,
+        end_time=end_time,
+        status=status,
+        host=request.user,
+        weight=TournamentWeights.GSC,
+        order=order,
+        token='',
+    )
+
+    return JsonResponse({'type': 'success', 'data': new_GSC})
+
+@require_POST
+def set_tournament(request: HttpRequest):
+    """
+    比赛的主办方可以修改比赛信息。目前仅支持修改部分信息，参见下文。
+
+    request应包含POST数据：
+        - tournament_id: 要更新的比赛ID
+        - start_time (可选): 比赛开始时间
+        - end_time (可选): 比赛结束时间
+        - order (可选, 仅GSC比赛): 届数
+        - token (可选, 仅GSC比赛): 比赛标识
+    """
+    if not (tournament_id := request.POST.get('tournament_id')):
+        return HttpResponseBadRequest()
+    if not (tournament := Tournament.objects.filter(id=tournament_id).first()):
+        return HttpResponseNotFound()
+    if tournament.host != request.user:
+        return HttpResponseForbidden()
+    
+    if start_time := request.POST.get('start_time'):
+        tournament.start_time = start_time
+    if end_time := request.POST.get('end_time'):
+        tournament.end_time = end_time
+
+    if isinstance(tournament, GSCTournament):
+        if order := request.POST.get('order'):
+            if GSCTournament.objects.filter(order=order).exists():
+                return HttpResponseConflict()
+            tournament.order = order
+        if token := request.POST.get('token'):
+            tournament.token = token
+
+    tournament.save()
+    return JsonResponse({'type': 'success', 'data': tournament})
+
+@require_POST
+@staff_required
+def set_tournament_staff(request: HttpRequest):
+    """
+    管理员可以修改比赛权重和比赛主办方。
+
+    request应包含POST数据：
+        - tournament_id: 要更新的比赛ID
+        - weight (可选): 比赛权重
+        - host_id (可选): 主办方用户ID
+    """
+    if not (tournament_id := request.POST.get('tournament_id')):
+        return HttpResponseBadRequest()
+    if not (tournament := Tournament.objects.filter(id=tournament_id).first()):
+        return HttpResponseNotFound()
+    
+    if (weight := request.POST.get('weight')):
+        tournament.weight = weight
+    if (host_id := request.POST.get('host_id')):
+        if not (host := UserProfile.objects.filter(id=host_id).first()):
+            return HttpResponseNotFound()
+        tournament.host = host
+    
+    tournament.save()
+    return JsonResponse({'type': 'success', 'data': tournament})
