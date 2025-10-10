0ea09c1545983dca2d4fead1ca9757df24eabbdc
Tianyi Pu <912396513@qq.com>
2025-10-10 11:40:32 +0800

    resolve conflict

--- back_end/saolei/identifier/views.py
+++ back_end/saolei/identifier/views.py
@@ -9,6 +9,8 @@ from utils.response import HttpResponseConflict
 from videomanager.models import VideoModel
 from videomanager.view_utils import update_personal_record_stock, update_state
 from .models import Identifier
+from msuser.models import UserMS
+from userprofile.models import UserProfile
 
 logger = logging.getLogger('userprofile')
 
@@ -67,29 +69,32 @@ def del_identifier(request):
     return JsonResponse({'type': 'success', 'object': 'identifier', 'category': 'add', 'value': len(video_list)})
 
 
-@require_POST
-@login_required_error
-def refresh_identifier(request: HttpRequest):
-    identifiers_new = list(Identifier.objects.filter(userms=request.user.userms, safe=True).values_list('identifier', flat=True))
-    identifiers_old = request.user.userms.identifiers
-    for identifier in identifiers_old:
-        if identifier not in identifiers_new:
-            for video in VideoModel.objects.filter(player=request.user, video__identifier=identifier):
-                if video.state == MS_TextChoices.State.OFFICIAL:
-                    update_state(video, MS_TextChoices.State.IDENTIFIER)
-    for identifier in identifiers_new:
-        if identifier not in identifiers_old:
-            for video in VideoModel.objects.filter(player=request.user, video__identifier=identifier):
-                if video.state == MS_TextChoices.State.IDENTIFIER:
-                    update_state(video, MS_TextChoices.State.OFFICIAL)
-    request.user.userms.identifiers = identifiers_new
-    request.user.userms.save()
-    return JsonResponse(identifiers_new, safe=False)
-
-
 # 管理员添加标识
-def staff_add_identifier(request):
-    # TODO
+@require_POST
+@staff_required
+def staff_add_identifier(request: HttpRequest):
+    if not (identifier_text := request.POST.get('identifier')):
+        return HttpResponseBadRequest('identifier')
+    if not (userid := request.POST.get('userid')):
+        return HttpResponseBadRequest('userid')
+    if not (user := UserProfile.objects.filter(id=userid).first()):
+        return HttpResponseNotFound('user')
+    if not (identifier := Identifier.objects.filter(identifier=identifier_text).first()):
+        identifier = Identifier.objects.create(identifier=identifier_text, safe=True, userms=user.userms)
+    if identifier.userms and identifier.userms.parent != user:
+        return HttpResponseForbidden()
+    identifier.userms = user.userms
+    identifier.save()
+    userms = user.userms
+    assert isinstance(userms, UserMS)
+    if identifier_text not in userms.identifiers:
+        userms.identifiers.append(identifier_text)
+        userms.save()
+    logger.info(f'管理员 #{request.user.id} 为用户 {user.realname}#{user.id} 添加标识 "{identifier_text}"')
+    video_list = VideoModel.objects.filter(player=user, video__identifier=identifier_text, state=MS_TextChoices.State.IDENTIFIER)
+    for video in video_list:
+        update_state(video, MS_TextChoices.State.OFFICIAL)
+    logger.info(f'修改了 {len(video_list)} 个录像状态')
     return HttpResponse()
 
 
