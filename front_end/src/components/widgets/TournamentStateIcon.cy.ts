c359cd6cba54688945e6e1e05ac40f39e27d4577
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2025-10-13 23:39:14 +0800

    组件测试：TournamentStateIcon (#378)
    * Create TournamentStateIcon.cy.ts
    
    * Update TournamentStateIcon.cy.ts

---
+++ front_end/src/components/widgets/TournamentStateIcon.cy.ts
@@ -0,0 +1,78 @@
+import TournamentStateIcon from './TournamentStateIcon.vue';
+import { TournamentState } from '@/utils/ms_const';
+import i18n from '@/i18n';
+
+describe('<TournamentStateIcon />', () => {
+    it('Pending', () => {
+        // see: https://on.cypress.io/mounting-vue
+        cy.mount(TournamentStateIcon, {
+            props: {
+                state: TournamentState.Pending,
+            },
+            global: {
+                plugins: [i18n],
+            },
+        });
+        cy.contains('Pending');
+    });
+
+    it('Preparing', () => {
+        cy.mount(TournamentStateIcon, {
+            props: {
+                state: TournamentState.Preparing,
+            },
+            global: {
+                plugins: [i18n],
+            },
+        });
+        cy.contains('Preparing');
+    });
+
+    it('Ongoing', () => {
+        cy.mount(TournamentStateIcon, {
+            props: {
+                state: TournamentState.Ongoing,
+            },
+            global: {
+                plugins: [i18n],
+            },
+        });
+        cy.contains('Ongoing');
+    });
+
+    it('Finished', () => {
+        cy.mount(TournamentStateIcon, {
+            props: {
+                state: TournamentState.Finished,
+            },
+            global: {
+                plugins: [i18n],
+            },
+        });
+        cy.contains('Finished');
+    });
+
+    it('Awarded', () => {
+        cy.mount(TournamentStateIcon, {
+            props: {
+                state: TournamentState.Awarded,
+            },
+            global: {
+                plugins: [i18n],
+            },
+        });
+        cy.contains('Awarded');
+    });
+
+    it('Cancelled', () => {
+        cy.mount(TournamentStateIcon, {
+            props: {
+                state: TournamentState.Cancelled,
+            },
+            global: {
+                plugins: [i18n],
+            },
+        });
+        cy.contains('Cancelled');
+    });
+});
