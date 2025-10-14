48ced05ac81ac4eb1b3735f543551d57eae10308
Tianyi Pu <912396513@qq.com>
2025-10-14 12:47:29 +0800

    feat: 个人主页录像列表支持自定义列

---
+++ front_end/src/components/widgets/MultiSelector.vue
@@ -0,0 +1,33 @@
+<template>
+    <el-checkbox-group v-model="selected">
+        <el-checkbox v-for="option in options" :key="option" :label="option" size="small">
+            {{ option }}
+        </el-checkbox>
+    </el-checkbox-group>
+    <el-tag v-for="option in selected" :key="option" closable size="small" round @close="handleClose">
+        {{ option }}
+    </el-tag>
+</template>
+
+<script setup lang="ts">
+
+import { ElCheckboxGroup, ElCheckbox, ElTag } from 'element-plus';
+import { PropType } from 'vue';
+
+defineProps({
+    options: {
+        type: Array as PropType<Readonly<string[]> | string[]>,
+        required: true,
+    },
+});
+
+const selected = defineModel({
+    type: Array as PropType<string[]>,
+    default: () => [],
+});
+
+function handleClose(tag: string) {
+    selected.value.splice(selected.value.indexOf(tag), 1);
+}
+
+</script>
--- front_end/src/views/PlayerVideosView.vue
+++ front_end/src/views/PlayerVideosView.vue
@@ -1,12 +1,15 @@
 <template>
     <UserArbiterCSV :id="store.player.id" />
-    <VideoList :videos="store.player.videos" :need-player-name="false" reverse height="800" sortable file-size />
+    <MultiSelector v-model="VideoListConfig.profile" :options="ColumnChoices" />
+    <VideoList :videos="store.player.videos" :columns="VideoListConfig.profile" sortable />
 </template>
 
 <script lang="ts" setup>
 // 个人主页的个人所有录像部分
-import VideoList from '@/components/VideoList.vue';
-import { store } from '../store';
+import VideoList from '@/components/VideoList/App.vue';
+import { store, VideoListConfig } from '../store';
 import UserArbiterCSV from '@/components/widgets/UserArbiterCSV.vue';
+import MultiSelector from '@/components/widgets/MultiSelector.vue';
+import { ColumnChoices } from '@/utils/ms_const';
 
 </script>
