4e898e550068c7ec375ee073b3da1abf9771479c
Tianyi Pu <912396513@qq.com>
2025-10-14 12:58:15 +0800

    设置可隐藏

--- front_end/src/views/PlayerVideosView.vue
+++ front_end/src/views/PlayerVideosView.vue
@@ -1,6 +1,12 @@
 <template>
-    <UserArbiterCSV :id="store.player.id" />
-    <MultiSelector v-model="VideoListConfig.profile" :options="ColumnChoices" />
+    <el-row>
+        <UserArbiterCSV :id="store.player.id" />
+        <span style="flex: 1" />
+        <el-button circle :type="showSetting ? 'primary' : 'default'" @click="showSetting = !showSetting">
+            <base-icon-setting />
+        </el-button>
+    </el-row>
+    <MultiSelector v-if="showSetting" v-model="VideoListConfig.profile" :options="ColumnChoices" />
     <VideoList :videos="store.player.videos" :columns="VideoListConfig.profile" sortable />
 </template>
 
@@ -11,5 +17,10 @@ import { store, VideoListConfig } from '../store';
 import UserArbiterCSV from '@/components/widgets/UserArbiterCSV.vue';
 import MultiSelector from '@/components/widgets/MultiSelector.vue';
 import { ColumnChoices } from '@/utils/ms_const';
+import BaseIconSetting from '@/components/common/BaseIconSetting.vue';
+import { ref } from 'vue';
+import { ElButton, ElRow } from 'element-plus';
+
+const showSetting = ref(false);
 
 </script>
