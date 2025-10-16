bde288e8845c5cee2351c6305a40918a44b5f81b
Tianyi Pu <912396513@qq.com>
2025-10-16 11:20:58 +0800

    GSC: 显示个人的总成绩

--- front_end/src/components/visualization/GSCPersonalSummary/App.vue
+++ front_end/src/components/visualization/GSCPersonalSummary/App.vue
@@ -1,27 +1,34 @@
 <template>
-    <el-row style="height: 1px" />
+    <el-row>
+        <AllSums :sum-time="sumTime" :sum-bvs="sumBvs" :sum-stnb="sumStnb" />
+    </el-row>
+    <el-row style="height: 20px" />
     <el-row>
         <span style="flex: 1" />
         <el-col :span="10" style="min-width: 16em">
-            <LevelBlock level="b" :videos="videos" />
+            <LevelBlock ref="BBlockRef" level="b" :videos="videos" />
         </el-col>
         <span style="flex: 1" />
         <el-col :span="10" style="min-width: 16em">
-            <LevelBlock level="i" :videos="videos" />
+            <LevelBlock ref="IBlockRef" level="i" :videos="videos" />
             <el-row style="height: 25px" />
-            <LevelBlock level="e" :videos="videos" />
+            <LevelBlock ref="EBlockRef" level="e" :videos="videos" />
         </el-col>
         <span style="flex: 1" />
     </el-row>
-    <el-row style="height: 1px" />
+    <el-row style="height: 20px" />
+    <el-row>
+        <AllSums :sum-time="sumTime" :sum-bvs="sumBvs" :sum-stnb="sumStnb" />
+    </el-row>
 </template>
 
 <script setup lang="ts">
 
-import { PropType } from 'vue';
+import { computed, PropType, ref } from 'vue';
 import LevelBlock from './LevelBlock.vue';
 import { VideoAbstract } from '@/utils/videoabstract';
 import { ElRow, ElCol } from 'element-plus';
+import AllSums from './AllSums.vue';
 
 defineProps({
     videos: {
@@ -30,15 +37,20 @@ defineProps({
     },
 });
 
+const BBlockRef = ref<InstanceType<typeof LevelBlock>>();
+const IBlockRef = ref<InstanceType<typeof LevelBlock>>();
+const EBlockRef = ref<InstanceType<typeof LevelBlock>>();
+
+const sumTime = computed(() => {
+    return (BBlockRef.value ? BBlockRef.value.sumAll.time : 0) + (IBlockRef.value ? IBlockRef.value.sumAll.time : 0) + (EBlockRef.value ? EBlockRef.value.sumAll.time : 0);
+});
+
+const sumBvs = computed(() => {
+    return (BBlockRef.value ? BBlockRef.value.sumAll.bvs : 0) + (IBlockRef.value ? IBlockRef.value.sumAll.bvs : 0) + (EBlockRef.value ? EBlockRef.value.sumAll.bvs : 0);
+});
+
+const sumStnb = computed(() => {
+    return (BBlockRef.value ? BBlockRef.value.sumAll.stnb : 0) + (IBlockRef.value ? IBlockRef.value.sumAll.stnb : 0) + (EBlockRef.value ? EBlockRef.value.sumAll.stnb : 0);
+});
+
 </script>
-
-<style lang="less" scoped>
-
-.levelblock {
-    width: 30%;
-    display: inline-block;
-    vertical-align: top;
-    min-width: 16em;
-}
-
-</style>
