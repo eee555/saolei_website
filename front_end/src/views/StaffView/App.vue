b7c0d900f022af6c317abc5971e141507e0314e2
Tianyi Pu <912396513@qq.com>
2026-04-14 09:15:04 +0200

    实现管理员页面子路由

--- front_end/src/views/StaffView/App.vue
+++ front_end/src/views/StaffView/App.vue
@@ -1,46 +1,45 @@
 <template>
-    <el-tabs v-model="activeTab">
-        <el-tab-pane label="用户信息" name="userprofile" lazy>
-            <StaffUserProfile />
-        </el-tab-pane>
-        <el-tab-pane label="录像信息" name="videomodel" lazy>
-            <StaffVideoModel />
-        </el-tab-pane>
-        <el-tab-pane label="账号绑定" name="accountlink" lazy>
-            <StaffAccountLink />
-        </el-tab-pane>
-        <el-tab-pane label="标识管理" name="identifier" lazy>
-            <StaffIdentifier />
-        </el-tab-pane>
-        <el-tab-pane label="录像批量更新" name="batchupdate" lazy>
-            <StaffBatchUpdateVideo />
-        </el-tab-pane>
-        <el-tab-pane label="后台日志" name="logs" lazy>
-            <StaffLogs />
-        </el-tab-pane>
-        <el-tab-pane label="比赛管理" name="tournament" lazy>
-            <StaffTournament />
-        </el-tab-pane>
-        <el-tab-pane label="后台任务" name="task" lazy>
-            <StaffTask />
-        </el-tab-pane>
+    <el-tabs :model-value="activeTab" @tab-click="handleTabClick">
+        <el-tab-pane v-for="tab in tabItems" :key="tab.name" :label="tab.label" :name="tab.name" />
+        <router-view v-slot="{ Component }">
+            <keep-alive>
+                <component :is="Component" />
+            </keep-alive>
+        </router-view>
     </el-tabs>
 </template>
 
 <script lang="ts" setup>
-// 管理员操作接口，通过'/staff'访问
 import { ElTabPane, ElTabs } from 'element-plus';
-import { ref } from 'vue';
+import { computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
-import StaffAccountLink from './AccountLink.vue';
-import StaffBatchUpdateVideo from './BatchUpdateVideo.vue';
-import StaffIdentifier from './Identifier.vue';
-import StaffLogs from './Logs.vue';
-import StaffTask from './Task.vue';
-import StaffTournament from './Tournament.vue';
-import StaffUserProfile from './UserProfile.vue';
-import StaffVideoModel from './VideoModel.vue';
+const route = useRoute();
+const router = useRouter();
 
-const activeTab = ref('userprofile');
+const tabItems = [
+    { name: 'userprofile', label: '用户信息' },
+    { name: 'videomodel', label: '录像信息' },
+    { name: 'accountlink', label: '账号绑定' },
+    { name: 'identifier', label: '标识管理' },
+    { name: 'batchupdate', label: '录像批量更新' },
+    { name: 'logs', label: '后台日志' },
+    { name: 'tournament', label: '比赛管理' },
+    { name: 'task', label: '后台任务' },
+] as const;
+const validTabs = tabItems.map((item) => item.name);
 
+// 根据当前路由路径计算激活的选项卡 name
+const activeTab = computed(() => {
+    const lastSegment = route.path.split('/').pop();
+    return (validTabs as string[]).includes(lastSegment!) ? lastSegment! : 'userprofile';
+});
+
+// 点击选项卡时跳转到对应子路由
+const handleTabClick = (tab: any) => {
+    const tabName = tab.paneName;
+    if (tabName) {
+        router.replace(`${tabName}`);
+    }
+};
 </script>
