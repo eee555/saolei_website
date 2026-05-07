d13d683a7ff18d94cf7f51deb7487644762a3168
Tianyi Pu <912396513@qq.com>
2026-05-07 08:23:00 +0200

    服务器页的api使用五分钟缓存

--- back_end/saolei/common/api.py
+++ back_end/saolei/common/api.py
@@ -1,5 +1,6 @@
 
 
+from django.core.cache import cache
 from django.db.models import Sum
 from django_tasks import TaskResultStatus
 from django_tasks_db.models import DBTaskResult
@@ -26,17 +27,19 @@ class VideoSummaryOut(Schema):
 
 @api.get('/videosummary', response=VideoSummaryOut, throttle=[AnonRateThrottle('10/m')])
 def api_video_summary(request):
+    if (cached_data := cache.get('api:common/videosummary')) is not None:
+        return cached_data
+
     total = VideoModel.objects.count()
-
     software = get_choice_counts_filtered(VideoModel, 'software', MS_TextChoices.Software)
-
     level = get_choice_counts_filtered(VideoModel, 'level', MS_TextChoices.Level)
-
     mode = get_choice_counts_filtered(VideoModel, 'mode', MS_TextChoices.Mode)
-
     state = get_choice_counts_filtered(VideoModel, 'state', MS_TextChoices.State)
 
-    return {'total': total, 'software': software, 'level': level, 'mode': mode, 'state': state}
+    result = {'total': total, 'software': software, 'level': level, 'mode': mode, 'state': state}
+    cache.set('api:common/videosummary', result, 300)
+
+    return result
 
 
 class TaskSummaryOut(Schema):
@@ -46,19 +49,29 @@ class TaskSummaryOut(Schema):
 
 @api.get('/tasksummary', throttle=[AnonRateThrottle('10/m')])
 def api_task_summary(request):
-    total = DBTaskResult.objects.count()
+    if (cached_data := cache.get('api:common/tasksummary')) is not None:
+        return cached_data
 
+    total = DBTaskResult.objects.count()
     status = get_choice_counts_filtered(DBTaskResult, 'status', TaskResultStatus)
 
-    return {'total': total, 'status': status}
+    result = {'total': total, 'status': status}
+    cache.set('api:common/tasksummary', result, 300)
+
+    return result
 
 
 @api.get('/diskusage', throttle=[AnonRateThrottle('10/m')])
 def api_disk_usage(request):
+    if (cached_data := cache.get('api:common/diskusage')) is not None:
+        return cached_data
+
     disk = psutil.disk_usage('.')
 
     video_size: int = VideoModel.objects.aggregate(s=Sum('file_size'))['s']
-
     db_size = get_db_size()
 
-    return {'total': disk.total, 'used': disk.used, 'free': disk.free, 'video': video_size, 'db': db_size}
+    result = {'total': disk.total, 'used': disk.used, 'free': disk.free, 'video': video_size, 'db': db_size}
+    cache.set('api:common/diskusage', result, 300)
+
+    return result
