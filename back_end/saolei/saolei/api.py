65e12ca294e17b9568428d3f94ed66eb03d5622b
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2026-05-10 18:06:27 +0200

    添加api: 用户头像 (#505)

--- back_end/saolei/saolei/api.py
+++ back_end/saolei/saolei/api.py
@@ -1,11 +1,13 @@
 from ninja import NinjaAPI, Redoc
 
 from common.api import router as common_router
+from userprofile.api import router as userprofile_router
 from utils.exceptions import ExceptionToResponse
 
 api = NinjaAPI(docs=Redoc())
 
 api.add_router('/common/', common_router)
+api.add_router('/userprofile/', userprofile_router)
 
 
 @api.exception_handler(ExceptionToResponse)
