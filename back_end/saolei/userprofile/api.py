76541c0bb93fed07bc6957a1fb173fa6b26b1d1c
Tianyi Pu <912396513@qq.com>
2026-05-12 12:04:51 +0200

    API: userprofile/info

--- back_end/saolei/userprofile/api.py
+++ back_end/saolei/userprofile/api.py
@@ -4,10 +4,40 @@ import os
 from django.http import FileResponse, HttpResponseNotFound
 from django.shortcuts import get_object_or_404
 from ninja import Router
+from ninja import File, Router, Schema, UploadedFile
 
 from .models import UserProfile
 
 router = Router()
+
+
+class UserInfoOut(Schema):
+    id: int
+    firstname: str
+    lastname: str
+    realname: str
+    signature: str
+    country: str
+    is_banned: bool
+    last_change_avatar: datetime
+    last_change_signature: datetime
+
+
+@router.get('/info', response=UserInfoOut)
+def get_user_info(request, user_id: int):
+    user = get_object_or_404(UserProfile, id=user_id)
+
+    return {
+        'id': user.id,
+        'firstname': user.firstname,
+        'lastname': user.lastname,
+        'realname': user.realname,
+        'signature': user.signature,
+        'country': user.country,
+        'is_banned': user.is_banned,
+        'last_change_avatar': user.last_change_avatar,
+        'last_change_signature': user.last_change_signature,
+    }
 
 
 @router.get('/avatar/{user_id}')
