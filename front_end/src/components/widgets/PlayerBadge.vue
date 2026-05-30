c8413734d533bd702c5dc796ecd978738f71d9a7
Tianyi Pu <912396513@qq.com>
2026-05-30 13:57:39 +0200

    fix

--- front_end/src/components/widgets/PlayerBadge.vue
+++ front_end/src/components/widgets/PlayerBadge.vue
@@ -1,14 +1,23 @@
 <template>
-    <UserAvatar :user-id="userId" style="height: 1em" />
-    <span>{{ name }}</span>
+    <UserAvatar :user-id="userId" style="height: 1em; margin-right: 0.25em" />
+    <span v-if="name === ''">
+        {{ t('common.anonymous') }}
+    </span>
+    <span v-else>
+        {{ name }}
+    </span>
 </template>
 
 <script setup lang="ts">
-import UserAvatar from './UserAvatar.vue';
+import { useI18n } from 'vue-i18n';
+
+import UserAvatar from '@/components/widgets/UserAvatar.vue';
 
 defineProps({
     userId: { type: Number, required: true },
     name: { type: String, default: '' },
 });
+
+const { t } = useI18n();
 
 </script>
