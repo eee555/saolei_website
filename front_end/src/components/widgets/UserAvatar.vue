e65bd22967b40635a5515d811d3021c7a889694d
Tianyi Pu <912396513@qq.com>
2026-05-30 08:16:01 +0200

    Create UserAvatar.vue

---
+++ front_end/src/components/widgets/UserAvatar.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-image :src="`${proxy.$axios.defaults.baseURL}/api/userprofile/avatar/${userId}`" style="max-height: 100%; max-width: 100%; aspect-ratio: 1 / 1;">
+        <template #error>
+            <img src="@/assets/person.png" style="max-height: 100%; max-width: 100%; aspect-ratio: 1 / 1;">
+        </template>
+    </el-image>
+</template>
+
+<script setup lang="ts">
+import useCurrentInstance from '@/utils/common/useCurrentInstance';
+
+defineProps({
+    userId: { type: Number, required: true },
+});
+
+const { proxy } = useCurrentInstance();
+
+</script>
