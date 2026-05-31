e935c296258f02ced017639130eb47a02d31a914
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2026-05-31 12:04:08 +0200

    重做录像播放弹窗 (#536)
    - 添加播放器选项
    - 显示录像id
    - flop-player背景支持深色模式 (https://gitee.com/ee55/saolei_website/issues/IAILIB)
    - flop-player窗口自动缩放 (#332)

---
+++ front_end/src/components/VideoPlayer/StrangeDust.vue
@@ -0,0 +1,52 @@
+<template>
+    <div v-if="!videoPlayerConfig.strangeDustTrust">
+        <el-result icon="warning">
+            <template #title>
+                {{ t('local.playerTooltip1') }}
+                <PlayerName :user-id="114" />
+                {{ t('local.playerTooltip2') }}
+            </template>
+            <template #sub-title>
+                <el-link href="https://strange-dust.github.io/minesweeper-replay-analyzer/" target="_blank">
+                    https://strange-dust.github.io/minesweeper-replay-analyzer/
+                </el-link>
+            </template>
+            <template #extra>
+                <el-button size="large" @click="videoPlayerConfig.strangeDustTrust = true">
+                    {{ t('local.ITrustIt') }}
+                </el-button>
+            </template>
+        </el-result>
+    </div>
+    <iframe v-else :src="`https://strange-dust.github.io/minesweeper-replay-analyzer/?replay=${src}`" style="width: calc(95vw - 50px); height: calc(100vh - 200px); background-color: transparent" />
+</template>
+
+<script setup lang="ts">
+import '@/styles/text.css';
+
+import { ElButton, ElLink, ElResult } from 'element-plus';
+import { useI18n } from 'vue-i18n';
+
+import PlayerName from '@/components/PlayerName.vue';
+import { videoPlayerConfig } from '@/store';
+
+defineProps({
+    src: { type: String, default: '' },
+});
+
+const i18nMessages = {
+    'zh-cn': { local: {
+        ITrustIt: '我信任该网站',
+        playerTooltip1: '此播放器来自',
+        playerTooltip2: '制作的第三方网站',
+    } },
+    'en': { local: {
+        ITrustIt: 'I trust the website',
+        playerTooltip1: 'This video player originated from a third-party website created by ',
+        playerTooltip2: ',',
+    } },
+};
+
+const { t } = useI18n({ messages: i18nMessages });
+
+</script>
