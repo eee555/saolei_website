377a9a2e4f7a624a87897103de2c46f0d42de487
Tianyi Pu <912396513@qq.com>
2026-06-02 20:09:10 +0200

    fix

--- front_end/cypress/e2e/profile.cy.ts
+++ front_end/cypress/e2e/profile.cy.ts
@@ -118,8 +118,7 @@ describe('Personal Profile', () => {
         cy.login(USERNAME, PASSWORD);
         cy.visitUser(USER_ID);
 
-        cy.get('.avatar').realHover({ position: 'center' });
-        cy.contains('高级sub200后才可以修改头像');
+        cy.get('.avatar').find('img').should('have.attr', 'title', '高级sub200后才可以修改头像');
     });
 
     it('Cannot update signature before sub200', () => {
@@ -160,15 +159,13 @@ describe('Personal Profile', () => {
         cy.login(USERNAME, PASSWORD);
         cy.visitUser(USER_ID);
 
-        cy.get('.avatar').realHover({ position: 'center' });
-        cy.contains('点击修改头像（剩余2次）');
+        cy.get('.avatar').find('img').should('have.attr', 'title', '点击修改头像（剩余2次）');
 
         cy.get('.avatar').find('input[type=file]').selectFile('cypress/fixtures/test.png', { force: true });
 
         cy.get('.avatar').find('img').should('have.attr', 'src').and('include', `${USER_ID}?v=1`);
 
-        cy.get('.avatar').realHover({ position: 'center' });
-        cy.contains('点击修改头像（剩余1次）');
+        cy.get('.avatar').find('img').should('have.attr', 'title', '点击修改头像（剩余1次）');
     });
 
     it('Update signature', () => {
