3f9c9da210ce3ce6381bcd49ec1c6c1a1e91c2b4
Tianyi Pu <912396513@qq.com>
2026-06-08 20:51:58 +0200

    为四个GET api允许CORS

---
+++ back_end/saolei/userprofile/handlers.py
@@ -0,0 +1,11 @@
+from corsheaders.signals import check_request_enabled
+from django.http import HttpRequest
+
+
+def cors_allow_api_userprofile(sender, request: HttpRequest, **kwargs):
+    p = request.path
+    if p.startswith('/api/userprofile/'):
+        return p.startswith('videolist', 17) or p.startswith('info/', 17) or p.startswith('identifier', 17) or p.startswith('avatar/', 17)
+
+
+check_request_enabled.connect(cors_allow_api_userprofile)
