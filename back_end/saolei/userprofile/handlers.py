c22bf6f6153d6d1f044aa2cfa8b3a0ab2b7821c1
Tianyi Pu <912396513@qq.com>
2026-06-08 20:59:25 +0200

    Update handlers.py

--- back_end/saolei/userprofile/handlers.py
+++ back_end/saolei/userprofile/handlers.py
@@ -7,5 +7,7 @@ def cors_allow_api_userprofile(sender, request: HttpRequest, **kwargs):
     if p.startswith('/api/userprofile/'):
         return p.startswith('videolist', 17) or p.startswith('info/', 17) or p.startswith('identifier', 17) or p.startswith('avatar/', 17)
 
+    return False
+
 
 check_request_enabled.connect(cors_allow_api_userprofile)
