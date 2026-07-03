f4d99966e3fc732a15080bf86ff01a0df31b9c93
Tianyi Pu <912396513@qq.com>
2026-07-03 09:54:29 +0200

    简化cache key

--- back_end/saolei/customranking/utils.py
+++ back_end/saolei/customranking/utils.py
@@ -1,5 +1,2 @@
-from .config import CUSTOM_PLUCK_CACHE_SIZE
-
-
 def get_custom_pluck_cache_key(level: str) -> str:
-    return f'customranking:pluck:{level}:top{CUSTOM_PLUCK_CACHE_SIZE}'
+    return f'customranking:pluck:{level}:top'
