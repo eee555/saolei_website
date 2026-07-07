e9a7dbab83da056b133e50edb7a9284318083457
👑➕🍋 <50390200+eee555@users.noreply.github.com>
2026-07-07 20:20:35 +0800

    chore：元扫雷3.3.2下载地址

--- front_end/src/components/dialogs/MetasweeperHelper.vue
+++ front_end/src/components/dialogs/MetasweeperHelper.vue
@@ -90,6 +90,25 @@ import { BaseFlagCN, BaseFlagDE, BaseFlagPL, BaseFlagUK } from '@/components/com
 
 const tableData = [
     {
+        version: 'v3.3.2',
+        date: '2026-7-7',
+        expire: '-',
+        links: [
+            {
+                label: 'OpenMS',
+                url: 'https://openms.top/download/Metasweeper-3.3.2.exe',
+            },
+            {
+                label: 'Gitee',
+                url: 'https://gitee.com/ee55/Metasweeper/releases/download/3.3.2/Metasweeper-3.3.2.exe',
+            },
+            {
+                label: 'GitHub',
+                url: 'https://github.com/eee555/Metasweeper/releases/download/3.3.2/Metasweeper-3.3.2.exe',
+            },
+        ],
+    },
+    {
         version: 'v3.3.1',
         date: '2026-6-25',
         expire: '-',
