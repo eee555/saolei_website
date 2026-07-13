01b0de03d0eced4be486bac9b1ca4be1d59e0090
Tianyi Pu <912396513@qq.com>
2026-07-13 21:15:49 +0200

    传错东西了

--- front_end/src/views/GuideView/routes.ts
+++
@@ -1,9 +0,0 @@
-import type { RouteRecordRaw } from 'vue-router';
-
-export const userRoutes: RouteRecordRaw[] = [
-    {
-        path: 'accountlink',
-        name: 'guide-accountlink',
-        component: () => import('./AccountLinks.vue'),
-    },
-];
